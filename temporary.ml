2d0295879059a701c167af318190d5d3eecb1e4b
ewan-delanoy <ewan.delanoy@gmx.fr>
2017-02-05 14:58:06 +0100

    added force_modification_time fun in Temporary module

--- temporary.ml
+++ temporary.ml
@@ -9,3 +9,5 @@ let cmd_for_tgt dir=German_command_for_ocaml_target.command_for_ocaml_target_in_
 let ingr_for_tgt dir=German_ingredients_for_ocaml_target.ingredients_for_ocaml_target;;
 let ingr_for_top dir=German_ingredients_for_ocaml_target.marked_ingredients_for_unprepared_toplevel;;
 
+let force_modification_time_update dir=
+  German_modify_modulesystem.force_modification_time_update;;
