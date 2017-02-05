7574fa510de2d6dda7a21fc53984769b95c1ba92
ewan-delanoy <ewan.delanoy@gmx.fr>
2017-02-05 14:47:01 +0100

    created a temporary module

---
+++ temporary.ml
@@ -0,0 +1,11 @@
+(*
+
+#use"temporary.ml";;
+
+*)
+
+
+let cmd_for_tgt dir=German_command_for_ocaml_target.command_for_ocaml_target_in_dir;;
+let ingr_for_tgt dir=German_ingredients_for_ocaml_target.ingredients_for_ocaml_target;;
+let ingr_for_top dir=German_ingredients_for_ocaml_target.marked_ingredients_for_unprepared_toplevel;;
+
