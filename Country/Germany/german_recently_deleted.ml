2289d43af1ea976e1b4607a38c938af088ee8e06
ewan-delanoy <ewan.delanoy@gmx.fr>
2017-02-06 09:22:34 +0100

    corrected German_recently_deleted module

--- Country/Germany/german_recently_deleted.ml
+++ Country/Germany/german_recently_deleted.ml
@@ -10,4 +10,4 @@
 let delete_file ap recently_deleted=
   let s_ap=Absolute_path.to_string ap in
   let s=Directory_name.cut_beginning German_constant.root s_ap in
-  Ordered.forget_order(Ordered_string.outsert s recently_deleted);; 
+  Ordered.forget_order(Ordered_string.insert s recently_deleted);; 
