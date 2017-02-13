36a1dfde5522daa35a87cf80845008cc044c329c
ewan-delanoy <ewan.delanoy@gmx.fr>
2017-02-13 14:32:47 +0100

    modified Gparser_constructor.

--- Kernighan/gparser_constructor.ml
+++ Kernighan/gparser_constructor.ml
@@ -84,11 +84,39 @@ module Private=struct
 
 let first_case_in_hwd 
   old_f (main_opener,main_closer,other_enclosers,s,i1,k,depth)=
-    old_f (main_opener,main_closer,other_enclosers,s,i1,k,depth,None);;
-
+   let opt1=Option.find_it(fun (opener,closer)->
+     Substring.is_a_substring_located_at opener s k
+   ) other_enclosers in
+   if opt1<>None
+   then let (op1,cl1)=Option.unpack opt1 in
+        old_f (main_opener,main_closer,other_enclosers,s,i1,k+(String.length op1),depth,Some(cl1))
+   else 
+   if Substring.is_a_substring_located_at main_opener s k
+   then old_f (main_opener,main_closer,other_enclosers,s,i1,k+(String.length main_opener),depth+1,None)
+   else 
+   if not(Substring.is_a_substring_located_at main_closer s k)
+   then old_f (main_opener,main_closer,other_enclosers,s,i1,k+1,depth,None)
+   else 
+   if depth>1
+   then old_f (main_opener,main_closer,other_enclosers,s,i1,k+(String.length main_closer),depth-1,None)
+   else 
+   let j1=k+(String.length main_closer) in
+   let descr=Gparser_description.house_with_doors (main_opener,main_closer) other_enclosers in
+   let res=Gparser_result.veil
+               descr
+               (i1,j1-1)
+               []
+               j1
+               None in
+   Some(res);;
+    
+    
+    
 let second_case_in_hwd 
-  old_f (main_opener,main_closer,other_enclosers,s,i1,k,depth,s_opt)=
-    old_f (main_opener,main_closer,other_enclosers,s,i1,k,depth,Some(s_opt)) ;;
+  old_f (main_opener,main_closer,other_enclosers,s,i1,k,depth,rparen)=
+  if Substring.is_a_substring_located_at rparen s k
+  then old_f(main_opener,main_closer,other_enclosers,s,i1,k+(String.length rparen),depth,None)
+  else old_f(main_opener,main_closer,other_enclosers,s,i1,k+1,depth,Some(rparen)) ;;
 
 let rec iterator_for_house_of_doors 
    (main_opener,main_closer,other_enclosers,s,i1,k,depth,opt)=
@@ -126,6 +154,11 @@ Gparser.apply (enclosure  ("ab","cde")) "ab345cde901" 1;;
 Gparser.apply (constant  "ab") "ab345cde901" 1;;
 Gparser.apply (simple_star  "ab") "ab345cde901" 1;;
 Gparser.apply (race  ("ab","b")) "123ab6ab90b234" 1;;
+Gparser.apply (house_with_doors ("(",")") ["ab","cd";"ef","gh"])
+				"(2(4)6(8)0)2(4(6)8(0)2)45" 2;;
+Gparser.apply (house_with_doors ("(",")") ["ab","cd";"ef","gh"])
+				"(23ab6(8)0)cd45gh89)12" 2;;
+
 
 paolo.xella@iscima.cnr.it
 
