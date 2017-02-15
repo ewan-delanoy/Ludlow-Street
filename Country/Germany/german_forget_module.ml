738ab1c9397b6d10af59b632670e5af2f0856e83
ewan-delanoy <ewan.delanoy@gmx.fr>
2017-02-15 10:00:38 +0100

    modified German_forget_module.

--- Country/Germany/german_forget_module.ml
+++ Country/Germany/german_forget_module.ml
@@ -18,7 +18,7 @@ let on_targets (mdata,dirs,tgts) hm=
     then let s_hm=Half_dressed_module.to_string hm in
          let fn=(Directory_name.to_string(German_constant.root))^s_hm in
          let _=Image.image
-         (fun edg->Shell_command.do_and_notice_failure("rm -f "^fn^edg^"*"))
-         [".cm";".d.cm";".caml_debuggable"] in
+         (fun edg->Shell_command.do_and_notice_failure("rm -f "^fn^edg))
+         [".ml";".mll";".mli";".mly";".cm*";".d.cm*";".caml_debuggable"] in
          German_unregister_module.on_targets (mdata,tgts) hm
     else raise(ModuleWithDependencies(hm,bel));;
