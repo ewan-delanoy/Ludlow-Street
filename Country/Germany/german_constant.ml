3e9ebc78f7a788023d05dd84072b2d94383ca168
ewan-delanoy <ewan.delanoy@gmx.fr>
2017-02-18 14:24:54 +0100

    modified German_constant.

--- Country/Germany/german_constant.ml
+++ Country/Germany/german_constant.ml
@@ -7,7 +7,7 @@
 *)
 
 
-let root="/Users/ewandelanoy/Documents/OCaml/Ordinary";;
+let root=Directory_name.of_string "/Users/ewandelanoy/Documents/OCaml/Ordinary";;
 let dir_for_backup=Directory_name.of_string 
 "/Users/ewandelanoy/Documents/OCaml/Githubbed_ocaml";;
 
