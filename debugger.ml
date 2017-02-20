c93098122a79641ddad9327301ef4dffdf771cf9
ewan-delanoy <ewan.delanoy@gmx.fr>
2017-02-20 09:30:08 +0100

    modified Debugger.

--- debugger.ml
+++ debugger.ml
@@ -6,6 +6,8 @@ Used for debugging purposes only.
 
 *)
 
+
+
 let ap1=Absolute_path.of_string
 ("/Users/ewandelanoy/Documents/Experimental_Installing/"^
 "Experimental_gnumake/010_Almost_cleaned/"^
@@ -14,3 +16,4 @@ let ap1=Absolute_path.of_string
 let text1=Io.read_whole_file ap1;;
 
 let test6=Gparser.apply  Gparser_for_c_language.main_prsr text1 1;;
+
