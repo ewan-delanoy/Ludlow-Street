6c082076e4e7cf3359a1bc31fa5c5ae64c358b98
ewan-delanoy <ewan.delanoy@gmx.fr>
2017-03-17 12:08:21 +0100

    modified Current_date.

--- current_date.ml
+++ current_date.ml
@@ -5,16 +5,10 @@
 *)
 
 let current_date ()=
-  let module Unix_again=(struct
-   include Unix;;
-   let day x=x.tm_mday;;
-   let month x=x.tm_mon;;
-   let year x=x.tm_year;;
-  end) in
   let temp=Unix.gmtime(Unix.time()) in
-  let year=string_of_int((Unix_again.year temp)+1900)
-  and month1=string_of_int((Unix_again.month temp)+1)
-  and day1=string_of_int(Unix_again.day temp) in
+  let year=string_of_int(temp.Unix.tm_year+1900)
+  and month1=string_of_int(temp.Unix.tm_mon+1)
+  and day1=string_of_int(temp.Unix.tm_mday) in
   let month=Cull_string.resize_from_right month1 2 '0'
   and day=Cull_string.resize_from_right day1 2 '0' in
   year^"_"^month^"_"^day;;
