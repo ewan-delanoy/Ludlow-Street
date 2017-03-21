ba25228dc923ada85fe7df6e63454db9db63d81e
ewan-delanoy <ewan.delanoy@gmx.fr>
2017-03-21 15:06:38 +0100

    created Ocaml_gsyntax_item.

---
+++ Ocaml_analysis/ocaml_gsyntax_item.ml
@@ -0,0 +1,28 @@
+(*
+
+#use"Ocaml_analysis/ocaml_gsyntax_item.ml";;
+
+*)
+
+
+(*
+type t={
+  name : string;
+  interval_for_name : int*int;
+  
+
+};;
+
+ prsr_for_value_making;
+       prsr_for_type_making;
+       prsr_for_exception_making;
+       prsr_for_comment;
+       prsr_for_sharp_comment;
+       prsr_for_special_sharp;
+       prsr_for_module_opener;
+       prsr_for_module_ender;
+       prsr_for_module_inclusion;
+       prsr_for_specialities;
+       prsr_for_white;
+       
+*)       
