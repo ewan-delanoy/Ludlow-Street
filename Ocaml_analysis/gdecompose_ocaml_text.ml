0b8fa3c29b7aaa601e43b137fa5639554aa54fb9
ewan-delanoy <ewan.delanoy@gmx.fr>
2017-03-23 16:21:36 +0100

    modified Gdecompose_ocaml_text.

--- Ocaml_analysis/gdecompose_ocaml_text.ml
+++ Ocaml_analysis/gdecompose_ocaml_text.ml
@@ -4,3 +4,36 @@
 
 *)
 
+
+exception Unreadable of string;;
+
+let accuse_final_excerpt s i=
+  let j=min(String.length s)(i+100) in
+  raise(Unreadable(Cull_string.interval s i j));;
+
+let read1 s=
+  let opt=Gparser.apply Gparser_for_ocaml_language.main_prsr s 1 in
+  if opt=None then accuse_final_excerpt s 1 else
+  let res=Option.unpack opt in 
+  let p=Gparser_result.final_cursor_position res in
+  if p<=(String.length s) 
+  then accuse_final_excerpt s p
+  else Gparser_result.important_ranges res;;
+  
+  
+
+
+(*
+
+let g1=German_wrapper.data();;
+let g2=List.filter Modulesystem_data.ml_present g1;;
+let g3=List.flatten (image Modulesystem_data.acolytes g2);;
+let g4=List.filter (fun mlx->snd(Mlx_filename.decompose mlx)=Ocaml_ending.ml) g3;;
+let g5=image Mlx_filename.to_absolute_path g4;;
+let g6=image (fun ap->let s=Io.read_whole_file ap in
+  (-(String.length s),(ap,s))
+) g5 ;;
+let g7=image snd (ofo(Tidel2.diforchan g6));;
+let g8=image (fun (ap,s)->read1 s) g7;;
+
+*)
