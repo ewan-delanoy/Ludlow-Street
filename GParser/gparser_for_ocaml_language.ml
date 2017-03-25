fb7b3bee84b475d0e636ad85dc70c77050a59973
ewan-delanoy <ewan.delanoy@gmx.fr>
2017-03-25 16:10:30 +0100

    modified Gparser_for_ocaml_language.

--- GParser/gparser_for_ocaml_language.ml
+++ GParser/gparser_for_ocaml_language.ml
@@ -115,6 +115,16 @@ let prsr_for_value_making=Gparser.Chain
      Gparser.Enclosure ("",double_semicolon);
    ];;
 
+let prsr_for_detailed_value_making=Gparser.Detailed_chain
+   [
+     Gparser.Constant "let";
+     prsr_for_white;
+     prsr_for_uncapitalized_word;
+     prsr_for_white_maybe;
+     Gparser.Enclosure ("","=");
+     Gparser.Enclosure ("",double_semicolon);
+   ];;
+
 let prsr_for_type_making=Gparser.Chain
    [
      Gparser.Constant "type";
