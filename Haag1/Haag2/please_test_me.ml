f76b1cedbe2f0e7b6550a905d2fb0514501670ad
ewan-delanoy <ewan.delanoy@gmx.fr>
2017-03-30 14:03:13 +0200

    modified Please_test_me.

--- Haag1/Haag2/please_test_me.ml
+++ Haag1/Haag2/please_test_me.ml
@@ -5,8 +5,24 @@
 *)
 
 
+module Mood=struct
+
+type mylabel= A |B |C |D |E;;
+
+type labelled_value={
+   label:mylabel;
+   value:int;
+};;
+
+let example={label=D;value=57;};;
+
+end;;
 
 
+let f x=match x.Mood.label with Mood.A|Mood.B|Mood.C->(1,x) 
+|Mood.D->(2,Mood.example) |Mood.E->(3,Mood.example);;
+
+(*
 module Mood=struct
 
 type mytype= A |B |C |D |E;;
@@ -19,5 +35,5 @@ let f x=match x with Mood.A|Mood.B|Mood.C->(1,x) |Mood.D->(2,Mood.D) |Mood.E->(3
 module Weak=struct
 let e=6;;
 end;;
-
+*)
 
