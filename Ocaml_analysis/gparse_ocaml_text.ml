78c3b393dd7c3a2bd90993d1a48785899cc5101b
ewan-delanoy <ewan.delanoy@gmx.fr>
2017-04-04 08:43:58 +0200

    modified Gparse_ocaml_text.

--- Ocaml_analysis/gparse_ocaml_text.ml
+++ Ocaml_analysis/gparse_ocaml_text.ml
@@ -190,7 +190,7 @@ let from_level2_to_level3 data_before (current_module,l)=
     iterator_from_level2_to_level3 
       ((data_before,current_module,Strung.split '.' current_module),l);;
 
-let read3 l_ap=
+let read_files l_ap=
    let temp1=Image.image( fun ap->
    let s_ap=Absolute_path.to_string ap
    and text=Io.read_whole_file ap in
@@ -209,6 +209,10 @@ let g2=List.filter Modulesystem_data.ml_present g1;;
 let g3=List.flatten (image Modulesystem_data.acolytes g2);;
 let g4=List.filter (fun mlx->snd(Mlx_filename.decompose mlx)=Ocaml_ending.ml) g3;;
 let g5=image Mlx_filename.to_absolute_path g4;;
+
+let g6=read3 g5;;
+
+
 let g6=image (fun ap->let s=Io.read_whole_file ap in
   (-(String.length s),(ap,s))
 ) g5 ;;
@@ -220,25 +224,6 @@ let g9=Explicit.image (fun (ap,l)->
 
 *)
 
-(*
-
-
-
-
- prsr_for_value_making;
-       prsr_for_type_making;
-       prsr_for_exception_making;
-       prsr_for_comment;
-       prsr_for_sharp_comment;
-       prsr_for_special_sharp;
-       prsr_for_module_opener;
-       prsr_for_module_ender;
-       prsr_for_module_inclusion;
-       prsr_for_specialities;
-       prsr_for_white;
-       
-*)       
-          
   
 (*  
 
