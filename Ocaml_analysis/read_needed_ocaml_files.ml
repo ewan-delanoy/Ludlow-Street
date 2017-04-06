4be616920944049e48594e574b95dbf350013746
ewan-delanoy <ewan.delanoy@gmx.fr>
2017-04-06 17:20:17 +0200

    modified Read_needed_ocaml_files.

--- Ocaml_analysis/read_needed_ocaml_files.ml
+++ Ocaml_analysis/read_needed_ocaml_files.ml
@@ -12,6 +12,8 @@ let read_needed_ocaml_files hm=
    let temp1=German_wrapper.data() in
    let md1=Option.find_really (fun md->Modulesystem_data.name md=hm) temp1 in
    let temp2=(Modulesystem_data.all_ancestors md1)@[hm] in
-   let all_files=Image.image  Modulesystem_data.ml_path in
+   let all_files=Image.image  (fun hm->
+   	 Mlx_filename.to_path(Mlx_filename.join hm Ocaml_ending.Ml)
+   ) temp2 in
    Read_ocaml_files.read_ocaml_files all_files;;
    
