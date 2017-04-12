84a221973dd53e77104b3fe3f40256d923966f68
ewan-delanoy <ewan.delanoy@gmx.fr>
2017-04-12 09:36:06 +0200

    modified Find_suitable_ending.

--- find_suitable_ending.ml
+++ find_suitable_ending.ml
@@ -13,11 +13,8 @@ Note that the order in Ocaml_ending.correspondances is important
 
 exception No_suitable_location of Directory_name.t*(Subdirectory.t list)*string;;
 
-let find_file_location dir l_subdir x=
-  let new_x=String.uncapitalize x in
-  let x_list=(if  new_x=x
-    then [x]
-    else [x;new_x]) in
+let find_file_location dir l_subdir old_x=
+  let x=String.lowercase old_x in
   let s_dir=Directory_name.to_string(dir) in
   let original_endings=Ocaml_ending.all_string_endings in
   let endings=(
@@ -25,12 +22,12 @@ let find_file_location dir l_subdir x=
      then [""]
      else original_endings
   ) in
-  let temp1=Cartesian.tproduct(l_subdir) x_list endings in
-  let tempf=(fun (sd,y,edg)->
-  let s1=s_dir^(Subdirectory.to_string sd)^y^edg in
-  if Sys.file_exists s1
-  then Some(Absolute_path.of_string s1)
-  else None
+  let temp1=Cartesian.product(l_subdir) endings in
+  let tempf=(fun (sd,edg)->
+  	let s1=s_dir^(Subdirectory.to_string sd)^x^edg in
+  	if Sys.file_exists s1
+  	then Some(Absolute_path.of_string s1)
+  	else None
   ) in
   let opt=Option.find_and_stop tempf temp1 in
   if opt=None
