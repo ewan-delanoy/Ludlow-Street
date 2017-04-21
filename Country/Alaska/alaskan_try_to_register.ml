6020030b4596e887cd29055dcac083524d4017fe
ewan-delanoy <ewan.delanoy@gmx.fr>
2017-04-21 11:39:21 +0200

    modified Alaskan_try_to_register.

--- Country/Alaska/alaskan_try_to_register.ml
+++ Country/Alaska/alaskan_try_to_register.ml
@@ -6,24 +6,39 @@
 *)
 
 
-
 let mlx_file mdata mlx_file=
     try(Some(Alaskan_register_mlx_file.on_monitored_modules 
         mdata mlx_file)) with _->None;;  
 
-let mlx_files mdata mlx_files=
-   let rec tempf=(fun
-    (vdata,failures,yet_untreated)->
-      match yet_untreated with
+module Private=struct
+
+exception Pusher_exn;;
+
+let pusher  (vdata,failures,yet_untreated)=
+     match yet_untreated with
+      []->raise(Pusher_exn)
+      |mlx::others->
+      (
+        match mlx_file vdata mlx with
+        None->(vdata,mlx::failures,others)
+        |Some(nfs)->(nfs,failures,others)
+      );; 
+
+let rec iterator x=
+   let (vdata,failures,yet_untreated)=x in
+   match yet_untreated with
       []->(failures,vdata)
       |mlx::others->
       (
         match mlx_file vdata mlx with
-        None->tempf(vdata,mlx::failures,others)
-        |Some(nfs)->tempf(nfs,failures,others)
-      )
-   ) in
-   tempf(mdata,[],mlx_files);;
+        None->iterator(pusher x)
+        |Some(nfs)->iterator(pusher x)
+      );;   
+
+end;;
+
+let mlx_files mdata mlx_files=
+   Private.iterator(mdata,[],mlx_files);;
  
   
    
