49ab64ad056b572fffe8108308847e165cb88f5e
ewan-delanoy <ewan.delanoy@gmx.fr>
2017-04-23 10:35:11 +0200

    modified German_update_copied_compiler.

--- Country/Germany/german_update_copied_compiler.ml
+++ Country/Germany/german_update_copied_compiler.ml
@@ -13,9 +13,29 @@ let prepare destdir=
   ) (German_wrapper.outside_files()) in
   let main_diff=Prepare_dircopy_update.compute_diff (German_constant.root,l1@l2) destdir in
   Prepare_dircopy_update.commands_for_update destdir main_diff;;
+
+let prepare_pervasives_file destdir=
+  let pervasives_file=Absolute_path.create_file(Directory_name.join destdir "my_pervasives.ml") in
+  let rep_text="\n\nSys.chdir(\""^(Directory_name.to_string destdir)^"\");;\n\n" in
+  Replace_inside.overwrite_between_markers_inside_file
+  (Overwriter.of_string rep_text)   
+  ("(* Directory setting starts here *)","(* Directory setting ends here *)")
+  pervasives_file;;
+
+let prepare_loadings_file destdir=
+  let loadings_file=Absolute_path.create_file(Directory_name.join destdir "my_loadings.ml") in
+  Replace_inside.replace_inside_file
+  (Directory_name.to_string German_constant.root,Directory_name.to_string destdir)
+  loadings_file;;
+
     
 let ucc destdir=
-  let _=Image.image Sys.command (prepare destdir) in 
+  let _=Image.image Sys.command (prepare destdir) in
+  let _=(
+    prepare_loadings_file destdir;
+    prepare_pervasives_file destdir
+  )  
+   in 
   Alaskan_create_target_system.from_main_directory destdir None [];;
        
        
