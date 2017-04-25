21c0299cdcd03dd8c88df24a97218ebe2a5f515e
ewan-delanoy <ewan.delanoy@gmx.fr>
2017-04-25 17:15:42 +0200

    modified Inclusion.

--- inclusion.ml
+++ inclusion.ml
@@ -22,4 +22,4 @@ let select_maximal_elements_carefully ll=
   Martian_partial_ordering.select_maximal_elements_carefully t ll;;
 
 let debbie=2+3*Please_test_me.Boogie.Woogie.barbecue;;
-let peggy=Please_test_me.b;;
+let peggy=Please_test_me.a;;
