f6fe5fcf8ba892e9e3d1900c6d4b673418dfd3cb
ewan-delanoy <ewan.delanoy@gmx.fr>
2017-04-26 18:15:20 +0200

    modified German_constant.

--- Country/Germany/german_constant.ml
+++ Country/Germany/german_constant.ml
@@ -15,7 +15,7 @@ let main_toplevel_name="ecaml";;
 let special_dirs=Image.image (fun sd->Subdirectory.SD "") ["Remembered";"Forgotten"];;
 
 
-let nume_for_makefile="makefile";;
+let nime_for_makefile="makefile";;
 let location_for_targetfile="targetfile.ocaml_made";;
 let location_for_loadingsfile="my_loadings.ml";;
 let location_for_pervasivesfile="my_pervasives.ml";;
