720427f8d4e361bb4c6f127facf158257aafdaa5
ewan-delanoy <ewan.delanoy@gmx.fr>
2017-05-02 19:44:51 +0200

    modified German_changed.

--- Country/Germany/german_changed.ml
+++ Country/Germany/german_changed.ml
@@ -10,7 +10,7 @@
 
 let update r_changed changed=
   let r_chan=Ordered_string.safe_set r_changed
-  and   chan=Ordered_string.safe_set changed in
+  and   chan=Ordered_string.safe_set (Recently_changed.to_string_list changed) in
   let whole=Ordered_string.teuzin r_chan chan in
-  Ordered.forget_order whole;;
+  Recently_changed.of_string_list(Ordered.forget_order whole);;
 
