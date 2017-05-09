69a88c4c97cc2390ffb0344bdad2e2278db3d207
ewan-delanoy <ewan.delanoy@gmx.fr>
2017-05-09 14:52:59 +0200

    modified Rename_endsubdirectory.

--- rename_endsubdirectory.ml
+++ rename_endsubdirectory.ml
@@ -7,10 +7,10 @@
 
 
 let re (old_subdir,new_esdname) s=
-   let s_old_subdir=Subdirectory.connectable_to_subpath old_subdir in
+   let s_old_subdir=Subdirectory.without_trailing_slash old_subdir in
    if Substring.begins_with s s_old_subdir
    then let sub_s=Cull_string.cobeginning (String.length s_old_subdir) s in
-        (Father_and_son.father s_old_subdir '/')^"/"^new_esdname^"/"^sub_s
+        (Father_and_son.father s_old_subdir '/')^"/"^new_esdname^sub_s
    else s;;
    
 (*
