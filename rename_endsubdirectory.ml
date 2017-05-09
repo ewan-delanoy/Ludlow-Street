c5751eeaad115f7c1b69d4640d6ec66a0372f50e
ewan-delanoy <ewan.delanoy@gmx.fr>
2017-05-09 14:46:36 +0200

    created Rename_endsubdirectory.

---
+++ rename_endsubdirectory.ml
@@ -0,0 +1,20 @@
+(*
+
+#use"rename_endsubdirectory.ml";;
+
+*)
+
+
+
+let re (old_subdir,new_esdname) s=
+   let s_old_subdir=Subdirectory.connectable_to_subpath old_subdir in
+   if Substring.begins_with s s_old_subdir
+   then let sub_s=Cull_string.cobeginning (String.length s_old_subdir) s in
+        (Father_and_son.father s_old_subdir '/')^"/"^new_esdname^"/"^sub_s
+   else s;;
+   
+(*
+
+re (Subdirectory.of_string("Haag/Huug"),"Java") ("Haag/Huug/King/Jordan/and_co.ml");;
+
+*)   
