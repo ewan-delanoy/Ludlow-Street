b5f2dbf99ad699b9e96ecdd039f7fcf7f8a13a73
ewan-delanoy <ewan.delanoy@gmx.fr>
2017-05-16 14:14:27 +0200

    modified Ocaml_gsyntax_item.

--- Ocaml_analysis/ocaml_gsyntax_item.ml
+++ Ocaml_analysis/ocaml_gsyntax_item.ml
@@ -8,6 +8,7 @@ type t={
   category : Ocaml_gsyntax_category.t;
   name : string;
   interval_for_name : int*int;
+  interval_for_introduction : int*int;
   content : string;
   interval_for_content : int*int;  
   is_an_included_item : bool;
@@ -17,11 +18,12 @@ let name x=x.name;;
 let content x=x.content;;
 
 
-let make cat nm nm_itv ctnt ctnt_itv incldd_or_not=
+let make cat nm nm_itv intr_itv ctnt ctnt_itv incldd_or_not=
     {
   		category =cat;
         name =nm;
         interval_for_name =nm_itv;
+        interval_for_introduction =nm_itv;
         content =ctnt;
         interval_for_content =ctnt_itv;  
         is_an_included_item =incldd_or_not;
@@ -32,6 +34,7 @@ let prepend_prefix prefix x=
   		category =x.category;
         name =prefix^"."^x.name;
         interval_for_name =x.interval_for_name;
+        interval_for_introduction =x.interval_for_introduction;
         content =x.content;
         interval_for_content =x.interval_for_content;  
         is_an_included_item =x.is_an_included_item;
@@ -42,6 +45,7 @@ let include_in_new_namespace new_nmspc x=
   		category =x.category;
         name =new_nmspc^(Father_and_son.invasive_father x.name '.');
         interval_for_name =x.interval_for_name;
+        interval_for_introduction =x.interval_for_introduction;
         content =x.content;
         interval_for_content =x.interval_for_content;  
         is_an_included_item =true;
