cfbf951855b31e3d211852b98e3da0b553615adf
ewan-delanoy <ewan.delanoy@gmx.fr>
2017-05-24 17:08:11 +0200

    deleted Old_php_recognizer.

--- Php_analizer/old_php_recognizer.ml
+++
@@ -1,9 +0,0 @@
-(*
-
-#use"Php_analizer/php_recognizer.ml";;
-
-*)
-
-type t=( Positioned_php_token.t list -> (Php_char_range.t * Positioned_php_token.t list) option );;
-
-let recognize (f:t) l=f l;;
