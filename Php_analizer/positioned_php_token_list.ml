23b01c4df6de0e7feed4f61e56ede47af5b3434c
ewan-delanoy <ewan.delanoy@gmx.fr>
2017-05-24 15:31:16 +0200

    modified Positioned_php_token_list.

--- Php_analizer/positioned_php_token_list.ml
+++ Php_analizer/positioned_php_token_list.ml
@@ -8,3 +8,11 @@ type t={
    contained : Positioned_php_token.t list;
 };;
 
+let empty={contained=[]};;
+let cons a x={contained=a::(x.contained)};;
+
+exception Ht_exn;;
+
+let ht x=match x.contained with
+    []->raise(Ht_exn)
+    |a::b->(a,{contained=b});;
