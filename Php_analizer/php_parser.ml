c1c10cf6d860656939c6cccf8e6ea888bf0d2302
ewan-delanoy <ewan.delanoy@gmx.fr>
2017-05-24 15:06:13 +0200

    modified Php_parser.

--- Php_analizer/php_parser.ml
+++ Php_analizer/php_parser.ml
@@ -5,8 +5,8 @@
 *)
 
 type 'a t= 
-((Positioned_php_token_list.t )->
-((('a)*Php_char_range.t*(Positioned_php_token_list.t)) option));;
+((Positioned_php_token.t list )->
+((('a)*Php_char_range.t*(Positioned_php_token.t list)) option));;
 
  
 let parse (f:'a t) l=f l;;  
