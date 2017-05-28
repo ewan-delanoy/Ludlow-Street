18bfc4d2930616d972a0009cd56b94023c609294
ewan-delanoy <ewan.delanoy@gmx.fr>
2017-05-28 16:18:36 +0200

    modified Php_check_recognizer_rules.

--- Php_analizer/php_check_recognizer_rules.ml
+++ Php_analizer/php_check_recognizer_rules.ml
@@ -8,22 +8,22 @@ If everything is OK, all the components of they_should_be_empty are []
 
 let ofo=Ordered.forget_order;;
 
-let part1=image 
+let part1=Image.image 
   Php_token.projected_version Php_token.fixture_of_nonconstants;;
 
-let z1=image (fun cst->Php_token.Constant(cst))
+let z1=Image.image (fun cst->Php_token.Constant(cst))
     Php_constant_token.all ;;
-let part2=image  Php_token.projected_version z1;;
+let part2=Image.image  Php_token.projected_version z1;;
 
-let part3=image fst Php_atomic_selector.special_list;;
+let part3=Image.image fst Php_atomic_selector.special_list;;
 
-let part4=image fst Php_short_selector.new_constants;;
+let part4=Image.image fst Php_short_selector.new_constants;;
 
 let part5=
    ofo(Tidel.diforchan(
-   (image fst Php_constructible_recognizer.all_pairs)
+   (Image.image fst Php_constructible_recognizer.all_pairs)
    @
-   (image snd Php_constructible_recognizer.all_pairs)
+   (Image.image snd Php_constructible_recognizer.all_pairs)
    ));;
    
 
@@ -35,14 +35,14 @@ let labelled_parts=[
    "recognizer",part5;
 ];;
 
-let parts=image snd labelled_parts;;
+let parts=Image.image snd labelled_parts;;
 
-let u1=image (fun x->(x,ofo(Tidel.diforchan(x))) ) parts;;
+let u1=Image.image (fun x->(x,ofo(Tidel.diforchan(x))) ) parts;;
 let check1=List.filter (fun (x,y)->hi(x)<>hi(y)) u1;;
-let u2=image snd u1;;
-let labelled_u2=image (fun ((x,y),z)->(x,z) ) (List.combine labelled_parts u2);;
+let u2=Image.image snd u1;;
+let labelled_u2=Image.image (fun ((x,y),z)->(x,z) ) (List.combine labelled_parts u2);;
 let whole=ofo(Tidel.diforchan(List.flatten u2));;
-let u3=image (
+let u3=Image.image (
   fun w->
    (w,Option.filter_and_unpack (
      fun (j,l)->
