99dcd07fe31a9c30eef79a56eb20ff471a7e9024
ewan-delanoy <ewan.delanoy@gmx.fr>
2017-06-02 15:03:09 +0200

    modified Termite_reverse_parse.

--- Php_analizer/termite_reverse_parse.ml
+++ Php_analizer/termite_reverse_parse.ml
@@ -7,21 +7,13 @@ Useful to debug the Termite.parse function.
 *)
 
 
-let rec iterator_for_reverse_parsing (graet,da_ober,l)=
-  match da_ober with
-  []->None
-  |(ret,wh)::da_ober2->
-     (
-       match Php_constructible_recognizer.recognize wh l with
-       None->Some(graet,wh,l,Php_constructible_recognizer.reverse_sleepy_parse wh l)
-       |Some(cr,peurrest)->
-          let d=Positioned_php_token_list.length(l)-Positioned_php_token_list.length(peurrest) in
-          let part=Positioned_php_token_list.big_head d l in
-          let graet2=(if ret=Glued_or_not.Not_retained_not_glued then graet else part::graet) in
-          iterator_for_reverse_parsing (graet2,da_ober2,peurrest)
-     );;
+let rec iterator_for_reverse_parsing x=
+  let y=Termite.pusher_for_parsing x in
+  if snd(y)<>None
+  then fst x
+  else iterator_for_reverse_parsing y;;
 
-let rp (Termite.Trmt(trmt)) l=iterator_for_reverse_parsing ([],trmt,l) ;;
+let rp (Termite.Trmt(trmt)) l=iterator_for_reverse_parsing (([],trmt,[],l),None) ;;
 
      
  
