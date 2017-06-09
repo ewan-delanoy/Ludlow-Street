6031e285f327562d23d6ec74c6dc8d876bbc55a5
ewan-delanoy <ewan.delanoy@gmx.fr>
2017-06-09 16:52:01 +0200

    created Slow_copy_task.

---
+++ slow_copy_task.ml
@@ -0,0 +1,108 @@
+(*
+
+#use"slow_copy_task.ml";;
+
+*)
+
+type t={
+   port    : int;
+   webhost : string;
+   local_root: string;
+   remote_root: string;
+   mutable filenames   :string list;  
+   keeper_name: string;
+};;
+
+
+let industrial_separator1=Industrial_separator.new_separator ();;
+let industrial_separator2=Industrial_separator.new_separator ();;    
+
+  
+  
+let archive x=
+   String.concat industrial_separator1
+   [
+     string_of_int(x.port);
+     x.webhost;
+     x.local_root;
+     x.remote_root;
+     Nonblank.make(String.concat industrial_separator2 x.filenames);
+     x.keeper_name;
+   ];;
+
+let unarchive s=
+   let l1=Str.split (Str.regexp_string industrial_separator1) s in
+{
+   port        =int_of_string(List.nth l1 0);
+   webhost     =List.nth l1 1;
+   local_root  =List.nth l1 2;
+   remote_root =List.nth l1 3;
+   filenames   =Str.split (Str.regexp_string industrial_separator2) (List.nth l1 4);  
+   keeper_name =List.nth l1 5;
+};;
+
+
+let initialize_from_file ap=unarchive(Io.read_whole_file ap);;
+
+let initialize_from_data
+  (p,w,l,r,dir,kn)=
+  let _=Sys.command("touch "^kn) in
+  let temp1=More_unix.complete_ls_with_nondirectories_only dir in
+  let d=String.length(l) in
+  let temp2=Image.image (fun ap->
+  Cull_string.cobeginning d (Absolute_path.to_string ap)) temp1 in
+  
+  {
+   	port        =p;
+   	webhost     =w;
+   	local_root  =l;
+   	remote_root =r;
+   	filenames   =temp2;  
+   	keeper_name =kn;
+   };;
+
+let save x=
+   let ap=Absolute_path.of_string(x.keeper_name) in
+   Io.erase_file_and_fill_it_with_string ap (archive x);;
+
+let execute_one_step x=
+   if x.filenames=[] then 0 else
+   let t=Single_copy_task.create_from_webhost_local_remote_file
+   (x.port,x.webhost,x.local_root,x.remote_root,List.hd(x.filenames)) in
+   let i=Single_copy_task.execute t in
+   let _=(
+   if i=0
+   then x.filenames<-List.tl(x.filenames);
+        save x
+   ) in
+   i;;   
+   
+let execute_all_steps x=
+   if x.filenames=[] then () else
+   let sn=string_of_int(List.length(x.filenames)) in
+   let counter=ref(1) in
+   while x.filenames<>[]
+   do
+   let i=execute_one_step x in
+   let message_end=(if i=0 then "succeeded" else "failed") in
+   let message="Step "^(string_of_int(!counter))^" of "^sn^" "^message_end in
+   let _=(if i=0 then counter:=(!counter)+1) in
+   print_string message;
+   flush stdout;
+   done;;  
+   
+(*
+
+let example=initialize_from_data
+  (7822,
+   "tribunem@tribunemicael.net",
+   "/Users/ewandelanoy/Documents/",
+   "~/private_html/",
+   Directory_name.of_string "/Users/ewandelanoy/Documents/Amailh",
+   "Remembered/task_keeper1");;
+
+
+*)
+     
+
+   
