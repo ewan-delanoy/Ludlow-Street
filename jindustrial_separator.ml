cb5bdb1d19cf3c68c4d21b5227dc336e82f36d21
ewan-delanoy <ewan.delanoy@gmx.fr>
2017-06-10 15:39:14 +0200

    created Jindustrial_separator. Modified My_printers.

---
+++ jindustrial_separator.ml
@@ -0,0 +1,14 @@
+(*
+
+#use"jindustrial_separator.ml";;
+
+Separators used in encoding of data types by strings.
+The main key should not appear in any string inside the data types,
+change it if necessary.
+
+*)
+
+let key="mpdykruvueaoqhkt";;
+
+
+  
