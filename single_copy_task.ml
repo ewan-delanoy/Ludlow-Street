2d2431c3cb2862917e1088e01b36a2660c67e4de
ewan-delanoy <ewan.delanoy@gmx.fr>
2017-06-11 10:43:47 +0200

    modified Single_copy_task.

--- single_copy_task.ml
+++ single_copy_task.ml
@@ -36,7 +36,7 @@ let execute x=
   let cmd2="scp -P "^(string_of_int x.port)^" \""^
   (x.local_root)^(x.filename)^"\" "^
   (x.webhost)^":"^
-  (x.remote_root)^(x.filename)^"" in
+  (x.remote_root)^(Charset.make_unix_compliant(x.filename)) in
   let i2=Sys.command cmd2  in
   if i2<>0 then 2 else
   0;;
