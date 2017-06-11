8be689f4ab1e0b07052aa93f89cb1c8426b36f58
ewan-delanoy <ewan.delanoy@gmx.fr>
2017-06-11 10:50:45 +0200

    modified Charset.

--- charset.ml
+++ charset.ml
@@ -92,9 +92,9 @@ let unix_rewrite_char c=
 exception Unix_unknown of char*string;;  
   
 let make_unix_compliant s=
-   try Image.image (fun j->
+   try String.concat "" (Image.image (fun j->
      String.make 1 (unix_rewrite_char(String.get s j))
-   ) (Ennig.ennig 0 (String.length(s)-1)) with
+   ) (Ennig.ennig 0 (String.length(s)-1))) with
    Unix_rewrite_exn(c)->raise(Unix_unknown(c,s));;  
   
   
