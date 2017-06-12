a891908e27625c356f05b83560e42f49d8742a51
ewan-delanoy <ewan.delanoy@gmx.fr>
2017-06-12 15:15:07 +0200

    modified Charset.

--- charset.ml
+++ charset.ml
@@ -86,20 +86,30 @@ let list_for_unix_usual=
 
 let list_for_unix_rewriting=
    [
-      " " ,"_";
-      "-" ,"_";
-      "'" ,"_single_quote_";
-      "\"","_single_quote_";
-      "&" ,"_and_";
-      "(" ,"_left_parenthesis_";
-      ")" ,"_right_parenthesis_";
-      "?" ,"_question_mark_";
-      "|" ,"_vertical_bar_";
-      "<" ,"_lower_than_";
-      ">" ,"_greater_than_";
-      "=" ,"_equals_";
-      "," ,"_comma_";
-      ";" ,"_semicolon_";
+                 " ","_";
+                 "-","_";
+                 "'","_single_quote_";
+                "\"","_double_quote_";
+                 "&","_and_";
+                 "(","_left_parenthesis_";
+                 ")","_right_parenthesis_";
+                 "?","_question_mark_";
+                 "|","_vertical_bar_";
+                 "<","_lower_than_";
+                 ">","_greater_than_";
+                 "=","_equals_";
+                 ",","_comma_";
+                 ";","_semicolon_";
+          "\xc2\xa0","_";
+          "\xcc\x80","_grave_";
+          "\xcc\x81","_acute_";
+          "\xcc\x83","_tilde_";
+          "\xcc\xa7","_cedilla_";
+      "\xe2\x80\x93","_";
+      "\xe2\x80\x94","_";
+      "\xe2\x80\x98","_lquote_";
+      "\xe2\x80\x99","_rquote_";
+      "\xe2\x80\xa6","_etc_";
     ];;
   
 let unix_rewrite_char t=List.assoc t
