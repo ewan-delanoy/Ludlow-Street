3d39f0a29aff46f7a00274ad877b501a94c7ca54
ewan-delanoy <ewan.delanoy@gmx.fr>
2017-06-18 09:12:53 +0200

    modified Unix_command.

--- unix_command.ml
+++ unix_command.ml
@@ -6,26 +6,34 @@ Wrapper on the Sys dot command function.
 
 *)
 
-(*
-let uc=Sys.command;;
-*)
-
 
 exception Command_failed of string;;
 
 let accu=ref([]:string list);;
+let remember_commands_mode=ref(false);;
+
 
 let hardcore_uc s=
    let i=Sys.command s in
    if i<>0
    then raise(Command_failed(s))
-   else let _=(accu:=s::(!accu)) in i;;
+   else let _=(if (!remember_commands_mode) then accu:=s::(!accu)) in 
+        i;;
+
+let mild_uc s=
+   let i=Sys.command s in
+   let _=(
+   if i<>0
+   then (print_string ("Failed during "^s);flush stdout)
+   else (if (!remember_commands_mode) then accu:=s::(!accu))
+   ) in
+   i;;
 
 let hardcore_mode=ref(true);;
 
 let uc s=
    if (!hardcore_mode)
    then hardcore_uc s
-   else Sys.command s;;
+   else mild_uc s;;
 
 
