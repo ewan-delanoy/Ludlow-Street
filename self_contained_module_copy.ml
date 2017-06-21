1ebc2c87c5f3752fd64d5ea76097bda2fe2f208f
ewan-delanoy <ewan.delanoy@gmx.fr>
2017-06-21 16:28:31 +0200

    modified Self_contained_module_copy.

--- self_contained_module_copy.ml
+++ self_contained_module_copy.ml
@@ -11,20 +11,22 @@ let self_contained_module_copy prefix hm=
          let naked_name=Modularize.module_name_from_path ap in
          let s_name=Naked_module.to_string naked_name in
          let mname=String.capitalize_ascii s_name in
-         let new_name=String.capitalize_ascii (prefix^s_name) in
-         let content=Io.read_whole_file ap
+         let new_mname=String.capitalize_ascii (prefix^s_name) in
+         let content=Io.read_whole_file ap in
          ((mname,new_mname),content)
    ) those_above in
-   let replacements=Image.image fst temp1 in
+   let replacements=Image.image (
+       fun ((a,b),_)->(Naked_module.of_string a,Naked_module.of_string b)
+   ) temp1 in
    let contents=Image.image snd temp1 in
    let new_contents=Image.image 
         (Look_for_module_names.change_several_module_names_in_string replacements  ) 
          contents in
    let temp2=List.combine replacements new_contents in
    let temp3=Image.image (
-     fun ((_n,new_mname),txt)->
-      "module "^new_mname^"=struct "^txt^" end;;"
-   ) in    
+     fun ((_,new_mname),txt)->
+      "module "^(Naked_module.to_string new_mname)^"=struct "^txt^" end;;"
+   ) temp2 in    
    String.concat "\n\n\n" temp3;;
 
 
