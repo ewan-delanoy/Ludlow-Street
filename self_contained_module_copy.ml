4edc1a70c5ccaff023442a22b4e03353c026cfe2
ewan-delanoy <ewan.delanoy@gmx.fr>
2017-06-21 20:06:02 +0200

    modified Self_contained_module_copy.

--- self_contained_module_copy.ml
+++ self_contained_module_copy.ml
@@ -2,6 +2,23 @@
 #use"self_contained_module_copy.ml";;
 *)
 
+
+let unsharped_content hm=
+    let mlx=Mlx_ended_absolute_path.join hm Ocaml_ending.ml in
+    let ap_ml=Mlx_ended_absolute_path.to_absolute_path mlx in
+    let naive_content=Io.read_whole_file ap in
+    if Sys.file_exists((Absolute_path.to_string ap_ml)^"l")
+    then (
+           let temp1=Str.split (Str.regexp_string "\n") naive_content in
+           let temp2=Option.filter_and_unpack (
+                fun t->
+                 if t="" then Some("") else
+                 if String.get t 0='#' then None else Some(t) 
+           ) temp1 in
+           String.concat "\n" temp2
+         )
+    else naive_content;;
+
 let self_contained_module_copy prefix hm=
    let those_above=German_data.above (German_wrapper.data()) hm in
    let temp1=Image.image (
@@ -12,7 +29,7 @@ let self_contained_module_copy prefix hm=
          let s_name=Naked_module.to_string naked_name in
          let mname=String.capitalize_ascii s_name in
          let new_mname=String.capitalize_ascii (prefix^s_name) in
-         let content=Io.read_whole_file ap in
+         let content=unsharped_content hm2 in
          ((mname,new_mname),content)
    ) those_above in
    let replacements=Image.image (
