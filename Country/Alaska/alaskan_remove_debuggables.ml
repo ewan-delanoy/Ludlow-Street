ffc87ed58826df7da358dfe408a4a2ea4b743d4b
ewan-delanoy <ewan.delanoy@gmx.fr>
2017-09-03 11:34:24 +0200

    modified Alaskan_remove_debuggables.

--- Country/Alaska/alaskan_remove_debuggables.ml
+++ Country/Alaska/alaskan_remove_debuggables.ml
@@ -7,7 +7,7 @@
 
 
 let rd dir mdata=
-   let sbuild=(Directory_name.connectable_to_subpath dir)^"_build" in
+   let sbuild=(Directory_name.connectable_to_subpath dir)^"_build/" in
    Unix_command.uc("rm -f "^sbuild^"*.d.cm*"^" "^sbuild^"*.ocaml_debuggable");;
    
   
