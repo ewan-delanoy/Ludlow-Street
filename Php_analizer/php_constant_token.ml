abc8870267291e75bdc04596478fbd265ab690bf
ewan-delanoy <ewan.delanoy@gmx.fr>
2017-09-07 17:11:37 +0200

    modified Php_constant_token.

--- Php_analizer/php_constant_token.ml
+++ Php_analizer/php_constant_token.ml
@@ -9,19 +9,16 @@ type t=
     |Punct of Php_punctuator.t
     |Op of Php_operator.t;;
 
-let make_visible=function
-      (Kwd kwd)->Php_keyword.make_visible kwd
-     |(Punct pkt)->Php_punctuator.make_visible pkt
-     |(Op op)->Php_operator.make_visible op;;
-  
-let all_pairs=
-       let kwds=Image.image (fun kwd->(Php_keyword.make_visible kwd,Kwd kwd)) Php_keyword.all
-       and puncts=Image.image (fun pkt->(Php_punctuator.make_visible pkt,Punct pkt)) Php_punctuator.all
-       and ops=Image.image (fun op->(Php_operator.make_visible op,Op op)) Php_operator.all in
-  Ordered.forget_order(Ordered.diforchan Keyval_ordering.ko  
-     (kwds@puncts@ops) );;
+let c_kwd kwd=Kwd(kwd);;
+let c_punct pkt=Punct(pkt);;
+let c_op op=Op(op);;    
 
-let all_string_constants=Image.image fst all_pairs;;
+let all_pairs=
+       let kwds=Image.image (fun kwd->(Php_keyword.make_visible kwd,c_kwd kwd)) Php_keyword.all
+       and puncts=Image.image (fun pkt->(Php_punctuator.make_visible pkt,c_punct pkt)) Php_punctuator.all
+       and ops=Image.image (fun op->(Php_operator.make_visible op,c_op op)) Php_operator.all in
+  Ordered.diforchan_plaen Keyval_ordering.ko  
+     (kwds@puncts@ops) ;;
 
 let all=Image.image snd all_pairs;;
 
@@ -31,3 +28,6 @@ let from_visible s=
    try List.assoc s all_pairs with
    _->raise(Unknown(s));;
 
+let make_visible tok=
+    let (s1,_)=Option.find_really(fun (s,tok1)->tok1=tok) all_pairs in
+    s1;;
