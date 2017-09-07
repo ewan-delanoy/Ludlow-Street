95ef77850b447adfa06d2045791d8790970ceff2
ewan-delanoy <ewan.delanoy@gmx.fr>
2017-09-07 13:42:07 +0200

    modified Php_punctuator.

--- Php_analizer/php_punctuator.ml
+++ Php_analizer/php_punctuator.ml
@@ -23,34 +23,40 @@ type t=
     |T_COMMA
     |T_COLON_COLON;;
 
-let to_string=function
-     T_LPARENTHESIS->"("
-    |T_RPARENTHESIS->")"
-    |T_LBRACE->"{"
-    |T_RBRACE->"}"
-    |T_ARROW->"->"
-    |T_SEMICOLON->";"
-    |T_COMMA->","
-    |T_COLON_COLON->"::";;
+    let t_lparenthesis = (T_LPARENTHESIS);;
+    let t_rparenthesis = (T_RPARENTHESIS);;
+    let t_comma = (T_COMMA);;
+    let t_arrow = (T_ARROW);;
+    let t_colon_colon = (T_COLON_COLON);;
+    let t_semicolon = (T_SEMICOLON);;
+    let t_lbrace = (T_LBRACE);;
+    let t_rbrace = (T_RBRACE);;
+ 
 
+let all_triples=[
+    (t_lparenthesis,"(","lparenthesis");
+    (t_rparenthesis,")","rparenthesis");
+    (t_comma,",","comma");
+    (t_arrow,"->","arrow");
+    (t_colon_colon,"::","colon_colon");
+    (t_semicolon,";","semicolon");
+    (t_lbrace,"{","lbrace");
+    (t_rbrace,"}","rbrace");
+ ];;
+ 
+ let to_string pkt=
+  let (_,viz,_)=Option.find_really(
+      fun (pkt1,_,_)->pkt1=pkt
+  ) all_triples in
+  viz;;
 
 
 
 
   let all_pairs =
     Ordered.forget_order( Ordered.diforchan Keyval_ordering.ko 
-    (Image.image (fun x->(to_string x,x))
-[
-     T_LPARENTHESIS;
-     T_RPARENTHESIS;
-     T_LBRACE;
-     T_RBRACE;
-     T_ARROW;
-     T_SEMICOLON;
-     T_COMMA;
-     T_COLON_COLON
+    (Image.image (fun (pkt,viz,sn)->(viz,pkt)) all_triples));;
 
-]));;
  
 let all_punctuators =Image.image snd all_pairs;; 
 
