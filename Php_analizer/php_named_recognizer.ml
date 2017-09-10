08e6854fac9b11e651aae1a0bb5d5f66b327fd77
ewan-delanoy <ewan.delanoy@gmx.fr>
2017-09-10 16:18:55 +0200

    created Php_named_recognizer. Modified Php_constructible_recognizer.

---
+++ Php_analizer/php_named_recognizer.ml
@@ -0,0 +1,162 @@
+(*
+
+#use"Php_analizer/php_named_recognizer.ml";;
+
+Adds names to PHP recognizers.
+
+*)
+
+type t=
+  {
+    name : string;
+    definition : string;
+    unnamed_content : Php_constructible_recognizer.t;
+  };;
+  
+  
+module Private=struct  
+
+    exception Name_too_long of string;;
+    exception Name_already_in_use of string;; 
+
+    let max_name_length=100;;
+    let data=ref(Image.image(
+      fun (s,sel)->{
+        name =s;
+        definition=s;
+        unnamed_content=Php_constructible_recognizer.Leaf(sel);
+      }
+    ) Php_short_selector.readables_and_selectors);; 
+    let automatic_name_counter=ref(0);;
+    
+    let new_automatic_name ()=
+         let j=(!automatic_name_counter)+1 in
+         let _=(automatic_name_counter:=j) in
+         "recognizer_"^(string_of_int(j));;
+
+    let compute_name (opt_name,definition)=
+        match opt_name with
+         Some(nahme)->(if String.length(nahme)>max_name_length
+                    then  raise(Name_too_long(nahme))
+                    else 
+                    if List.exists(fun x->x.name=nahme)(!data)
+                    then raise(Name_already_in_use(nahme))
+                    else nahme)
+        |None->if (String.length(definition)>max_name_length)     
+                  ||
+                  (List.exists(fun x->x.name=definition)(!data))
+               then new_automatic_name ()
+               else definition;;     
+
+    let make (opt_name,defn,rcgzr)=
+         let x={
+          name =compute_name (opt_name,defn);
+          definition=defn;
+          unnamed_content=rcgzr;
+         }  in
+         let _=(data:=(x::(!data))) in
+         x;; 
+    
+    let generalized opt_name grlzr nr=
+          let (lpar,rpar)=Generalizer.pair grlzr in
+          let definition=lpar^(nr.name)^rpar in
+          let rcgzr=Php_constructible_recognizer.Generalized
+             (grlzr,nr.unnamed_content) in  
+          make (opt_name,definition,rcgzr);;   
+             
+    let chain opt_name l_nr=
+          let definition=String.concat " " (Image.image (fun nr->nr.name) l_nr) in
+          let rcgzr=Php_constructible_recognizer.Chain
+          (Image.image (fun nr->nr.unnamed_content) l_nr) in  
+           make (opt_name,definition,rcgzr);;   
+        
+    let disjunction opt_name l_nr=     
+      let (lpar,rpar)=Php_constructible_recognizer.pair_for_disjunction in
+      let definition=
+        lpar^
+        (String.concat Php_constructible_recognizer.associator_for_disjunction 
+          (Image.image (fun nr->nr.name) l_nr))
+        ^rpar
+      in
+      let rcgzr=Php_constructible_recognizer.Chain
+      (Image.image (fun nr->nr.unnamed_content) l_nr) in  
+       make (opt_name,definition,rcgzr);;   
+
+    exception Unknown_name of string;;
+
+    let of_name nahme=
+      try Option.find(fun nr->nr.name=nahme)(!data) with
+      Option.Unpackable(_)->raise(Unknown_name(nahme));;
+
+    exception Helper_for_definition_reading_exn of ((string*string) option)*string;;
+    
+    
+    let helper_for_definition_reading (opt,t)=
+                if opt=None then of_name t else
+                let pair=Option.unpack opt in
+                let opt2=Option.seek
+                  (fun x->(Generalizer.pair x)=pair)
+                  Generalizer.all in
+                if opt2<>None 
+                then generalized None (Option.unpack opt2) (of_name t) 
+                else
+                if pair=Php_constructible_recognizer.pair_for_disjunction
+                then 
+                     let temp1=Parenthesed_block.decompose_with_associator
+                                Php_constructible_recognizer.associator_for_disjunction 
+                                Php_constructible_recognizer.all_pairs t in
+                     disjunction None (Image.image of_name temp1)
+                else
+                raise(Helper_for_definition_reading_exn(opt,t));; 
+    
+    exception Empty_output;;
+    exception Complicated of string*((((string*string) option)*string) list);; 
+        
+                
+    let of_definition rough_s=
+        let s=Cull_string.trim_spaces rough_s in
+        if s="" then raise(Empty_output) else
+        let temp1=Parenthesed_block.decompose_without_taking_blanks_into_account 
+           Php_constructible_recognizer.all_pairs s in
+        let temp2=Image.image (fun (opt,t)->(opt,Cull_string.trim_spaces t) ) temp1 in
+        let temp3=List.filter (fun (opt,t)->t<>"") temp2 in
+        if List.length(temp3)>1
+        then raise(Complicated(s,temp3))
+        else 
+        if temp3=[]
+        then raise(Empty_output)
+        else
+        let (opt,t)=List.hd temp3 in
+        if opt<>None
+        then helper_for_definition_reading (opt,t)
+        else
+        let l_sel=Php_short_selector.list_from_string t in
+        let rcgzr=(
+           if List.length(l_sel)=1
+           then Php_constructible_recognizer.Leaf(List.hd l_sel)
+           else let l=Image.image (fun sel->Php_constructible_recognizer.Leaf(sel)) l_sel in
+           Php_constructible_recognizer.Chain(l)
+         ) in
+        make (None,s,rcgzr);;            
+
+end;;
+
+
+let generalized=Private.generalized;;
+let chain=Private.chain;;
+let disjunction=Private.disjunction;;
+
+let of_name=Private.of_name;;
+let of_definition=Private.of_definition;;
+
+
+let print (x:t)=
+    "\xc9\xbe  "^(x.name)^"  \xc9\xbf";;
+  
+let print_out (fmt:Format.formatter) (x:t)=
+     Format.fprintf fmt "@[%s@]" (print x);;
+
+
+(*
+
+*)
