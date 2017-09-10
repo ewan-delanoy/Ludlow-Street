40ffbded86b92938c4060a7a784bbcdb4feac9f1
ewan-delanoy <ewan.delanoy@gmx.fr>
2017-09-10 06:39:09 +0200

    modified Php_positioned_token_list.

--- Php_analizer/php_positioned_token_list.ml
+++ Php_analizer/php_positioned_token_list.ml
@@ -4,44 +4,42 @@
 
 *)
 
-type t={
-   contained : ( Php_token.t * (Lexing.position * Lexing.position)  ) list;
-};;
+type t= ( Php_token.t * (Lexing.position * Lexing.position)  ) list;;
 
-let empty={contained=[]};;
-let is_empty x=(x.contained=[]);;
-let hd x=List.hd(x.contained);;
-let tl x={contained=List.tl(x.contained)};;
-let concat x y={contained=(x.contained)@(y.contained)};;
+let empty=([]:t);;
+let is_empty (x:t)=(x=[]);;
+let hd (x:t)=List.hd(x);;
+let tl (x:t)=List.tl(x);;
+let concat (x:t) (y:t)=(x@y:t);;
 
-let cons a x={contained=a::(x.contained)};;
-let singleton x={contained=[x]};;
-let rev x={contained=List.rev(x.contained)};;
-let length x=List.length(x.contained);;
-let big_head d x={contained=Listennou.big_head d (x.contained)};;
+let cons a (x:t)=(a::x:t);;
+let singleton (x:t)=[x];;
+let rev (x:t)=(List.rev(x):t);;
+let length (x:t)=List.length(x);;
+let big_head d (x:t)=Listennou.big_head d (x);;
 
-let filter f x={contained=List.filter f (x.contained)}
+let filter f x=List.filter f x;;
 
 exception Ht_exn;;
 
-let ht x=match x.contained with
+let ht x=match x with
     []->raise(Ht_exn)
-    |a::b->(a,{contained=b});;
+    |a::b->(a,b);;
     
 exception File_exn;;    
     
-let file x=match x.contained with
+let file x=match x with
     []->raise(File_exn)
     |(_,(y1,_))::_->y1.Lexing.pos_fname;;    
     
-let print x=
+let print (x:t)=
   let temp1=Image.image(fun ptok->
     let tok=fst ptok in
     Php_projected_token.readable(Php_token.form tok)
-   ) x.contained in
+   ) x in
   "\xe3\x80\x90  "^(String.concat " " temp1)^"  \xe3\x80\x91";;
 
-let print_out (fmt:Format.formatter) x=
+let print_out (fmt:Format.formatter) (x:t)=
    Format.fprintf fmt "@[%s@]" (print x);;
 
     
