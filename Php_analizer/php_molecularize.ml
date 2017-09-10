52d363d9c019572adb1599808381dc6795661a84
ewan-delanoy <ewan.delanoy@gmx.fr>
2017-09-10 11:10:17 +0200

    modified Level_one,Php_molecularize.

--- Php_analizer/php_molecularize.ml
+++ Php_analizer/php_molecularize.ml
@@ -17,7 +17,7 @@ let rec iterator_for_molecularization
       (
         let (tok,lxg)=a in
          match Php_blocker_name.seek_block_beginning tok with
-         None->iterator_for_molecuralization((Php_molecule.of_token tok,lxg)::graet,da_ober)
+         None->iterator_for_molecularization((Php_molecule.of_token tok,lxg)::graet,da_ober)
         |Some(blckr)->
            let opt=Php_recognize_starting_block.rsb blckr da_ober in
            if opt=None then raise(Molecularization_exn) else
@@ -25,7 +25,7 @@ let rec iterator_for_molecularization
            let d=List.length(da_ober)-List.length(l2) in
            let l3=Listennou.big_head d da_ober in
            let mole=Php_molecule.block blckr l3 in
-           iterator_for_molecuralization((mole,Php_char_range.unveil cr)::graet,l2)
+           iterator_for_molecularization((mole,Php_char_range.unveil cr)::graet,l2)
       );;
 
 end;;
