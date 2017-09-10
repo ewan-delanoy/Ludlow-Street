a35f7b41148fd8d0d530a0c31ee7b579734c6c14
ewan-delanoy <ewan.delanoy@gmx.fr>
2017-09-10 10:57:40 +0200

    modified Php_molecule.

--- Php_analizer/php_molecule.ml
+++ Php_analizer/php_molecule.ml
@@ -10,6 +10,10 @@ type t= [Php_token.t|`Block of Php_blocker_name.t*(Php_positioned_token_list.t)]
 
 let of_token (tok:Php_token.t)=(tok:>t);;
 let block blckr l=((`Block(blckr,l)):t);;
+let uniformize ((mole:t),cr)=match mole with
+   #Php_token.t as tok->[tok,cr]
+  |`Block(_,l)->l;;
+
 
 let readable (mole:t)=match mole with
    #Php_token.t as tok->Php_token.readable(tok)
