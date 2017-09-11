52c70a5b887a91413ddfdf97c19e61f102f92ef5
ewan-delanoy <ewan.delanoy@gmx.fr>
2017-09-11 14:49:57 +0200

    modified Keyval_ordering,Php_projected_token,Beaver_for_statement.

--- keyval_ordering.ml
+++ keyval_ordering.ml
@@ -6,5 +6,5 @@
 
 
 let ko=((fun x y->Total_ordering.product 
-  Dictionary_order.dictionary_order Total_ordering.standard x y): 
+  Total_ordering.dictionary_order Total_ordering.standard x y): 
   (string * 'a) Total_ordering.t);;
