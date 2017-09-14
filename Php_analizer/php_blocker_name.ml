bcaea70b7d41cc33446ed4513d636b7163702c22
ewan-delanoy <ewan.delanoy@gmx.fr>
2017-09-14 16:15:15 +0200

    modified Php_lexer,Php_molecule,Php_blocker_name,Php_projected_token_set.

--- Php_analizer/php_blocker_name.ml
+++ Php_analizer/php_blocker_name.ml
@@ -8,6 +8,8 @@
 
 type t=Among of Php_constant_token.t * Php_constant_token.t;; 
 
+let unveil (Among(x,y))=(x,y);;
+
 let parenthesis=Among(`T_LPARENTHESIS, `T_RPARENTHESIS);;
 let brace=Among(`T_LBRACE, `T_RBRACE);;
 let bracket=Among(`T_LBRACKET, `T_RBRACKET);;
@@ -15,6 +17,8 @@ let ternop=Among(`T_QUESTION, `T_COLON);;
 
 let all=
   [parenthesis;brace;bracket;ternop];;  
+
+
 
 let make_visible (Among(x,y))=
   (Php_constant_token.make_visible x,
