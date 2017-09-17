5885c2034a51b8736fbaffea3f4bb4d6e85c5955
ewan-delanoy <ewan.delanoy@gmx.fr>
2017-09-17 09:34:05 +0200

    modified Recreating_tools.

--- recreating_tools.ml
+++ recreating_tools.ml
@@ -4,6 +4,8 @@
 
 *)
 
+
+
 let encode_projtok x=Listennou.find_index x Php_projected_token.all_tokens;;
 let decode_projtok i=List.nth Php_projected_token.all_tokens (i-1);;
 
@@ -35,12 +37,48 @@ let decode_postok  (i,s,t,j1,j2,j3,k1,k2,k3)=
 let encode_postok_list=Image.image encode_postok;;
 let decode_postok_list=Image.image decode_postok;;
 
+let encode_projtok_set (Php_projected_token_set.N l)=Image.image encode_projtok l;;
+let decode_projtok_set l=Php_projected_token_set.N(Image.image decode_projtok l);;
 
+let encode_generalizer blckr=Listennou.find_index blckr Generalizer.all;;
+let decode_generalizer k=List.nth Generalizer.all (k-1);;
 
+let encode_blocker blckr=Listennou.find_index blckr Php_blocker.all;;
+let decode_blocker k=List.nth Php_blocker.all (k-1);;
 
+let encode_short_sel=function
+  Php_short_selector.Atomic(pts)->(1,encode_projtok_set pts,0,0)
+| Php_short_selector.Block(blckr)->(2,[],encode_blocker blckr,0)
+| Php_short_selector.Unusual_block(blckr,d)->(3,[],encode_blocker blckr,d);;
 
+let decode_short_sel (i_pattern,i_projtok,i_blocker,depth)=
+   match i_pattern with
+    1->Php_short_selector.Atomic(decode_projtok_set i_projtok)
+   |2->Php_short_selector.Block(decode_blocker i_blocker)
+   |_->Php_short_selector.Unusual_block(decode_blocker i_blocker,depth);;
 
-     
+let dummy_short_sel=(4,[],0,0);;   
+
+exception Enc_exn of Php_constructible_recognizer.t;;
+exception Dec_exn of Php_constructible_recognizer.t;;
+
+let encode1_cons_recgzr=function
+   x->raise(Enc_exn(x));;   
+let decode1_cons_recgzr=function
+   x->raise(Dec_exn(x));;   
+
+let encode0_cons_recgzr=function
+ Php_constructible_recognizer.Leaf (ssel)->(1,encode_short_sel ssel,0,[])  
+| Php_constructible_recognizer.Generalized (grzr,x)->(2,dummy_short_sel,encode_generalizer grzr,[encode1_cons_recgzr x])                                      
+| Php_constructible_recognizer.Chain(l)->(3,dummy_short_sel,0,Image.image encode1_cons_recgzr l)
+| Php_constructible_recognizer.Disjunction(l)->(4,dummy_short_sel,0,Image.image encode1_cons_recgzr l);;
+
+let decode0_cons_recgzr (i_pattern,e_sel,i_grzr,e_older)=
+   match i_pattern with
+   1->Php_constructible_recognizer.Leaf (decode_short_sel e_sel)
+  |2->Php_constructible_recognizer.Generalized (decode_generalizer i_grzr,decode1_cons_recgzr (List.hd e_older))
+  |3->Php_constructible_recognizer.Chain(Image.image decode1_cons_recgzr e_older)                                      
+  |_->Php_constructible_recognizer.Disjunction(Image.image decode1_cons_recgzr e_older);;
 
 
 
