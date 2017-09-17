586dcf96d2010fb24449b04a2440705b68c89829
ewan-delanoy <ewan.delanoy@gmx.fr>
2017-09-17 14:59:28 +0200

    modified Php_spider.

--- Php_analizer/php_spider.ml
+++ Php_analizer/php_spider.ml
@@ -15,64 +15,92 @@ let unveil (Sp l)=l;;
 
 (* Definition of PHP spider begins here *)
 
-let php_ref=ref(Sp [
-
+let php_ref=ref(Sp[
   "optional_pblock",["_l_ () _r?_"];
+
   "namespace_name",["_l_ id _u_ nmspc _rd_"];
+
   "assignable",[
-    "coerce           id ()";
-    "nmspc            _l_ :: id _r?_ optional_pblock";
-    "id ::            id ()";
-    "id () ?: no_semicolon";
-    "id () .          sqs";
-    "id ()            ";
-    "hdoc ";
-    "include_like     _l_ loose= _r*_ ";
-    "int          ";
-    "new id           ()";
-    "new nmspc        ()";
-    "sqs . vvar . dqs . vvar -> id () . dqs . vvar -> id () . dqs";
-    "sqs . vvar . sqs";
-    "sqs";
-    "vvar [ sqs ]";
-    "vvar . sqs";
-    "vvar = sqs";
-    "vvar -> id optional_pblock _l_ -> id optional_pblock _r*_";
-    "vvar + _l_ loose= _r*_ ";
-    "vvar";
-    "@                id ()";
-    "() ?  string_or_var  :  string_or_var  "
+                 "coerce           id ()";
+                 "nmspc            _l_ :: id _r?_ optional_pblock";
+                 "id ::            id ()";
+                 "id () ?: no_semicolon";
+                 "id () .          sqs";
+                 "id ()            ";
+                 "hdoc ";
+                 "include_like     _l_ loose= _r*_ ";
+                 "int          ";
+                 "new id           ()";
+                 "new nmspc        ()";
+                 "sqs . vvar . dqs . vvar -> id () . dqs . vvar -> id () . dqs";
+                 "sqs . vvar . sqs";
+                 "sqs";
+                 "vvar [ sqs ]";
+                 "vvar . sqs";
+                 "vvar = sqs";
+                 "vvar -> id optional_pblock _l_ -> id optional_pblock _r*_";
+                 "vvar + _l_ loose= _r*_ ";
+                 "vvar";
+                 "@                id ()";
+                 "() ?  string_or_var  :  string_or_var  "
                ];
-    "beheaded_ivy",[
-      "exit ;";
-      "{}   _l_ else if () {} _r*_      else {} ";
-    ];
-    "beheaded_iwy",[
-      "_l_ no_ivies _r*_ if () : _l_ no_ivies _r*_ else : _l_ no_ivies _r*_ endif ; _l_ no_ivies _r*_";
-      "_l_ no_ivies _r*_ if () : _l_ no_ivies _r*_ endif ; _l_ no_ivies _r*_";
-      "_l_ no_ivies _r*_"
-    ];  
-    "statement",[
-      "vvar [ ] = assignable ;"; "vvar assign & assignable ;";                                                "vvar -> id_or_var  =  assignable ;"; "id :: id_or_var =  assignable ;";                             
-      "vvar assign  assignable ;";
-      "vvar [  int_or_string_or_var  ] = assignable ;";
-      "vvar [  int_or_string_or_var   ]  =   & assignable  ;";
-      "abstract class _l_ no_left_brace _r*_ {}";
-      "final class _l_ no_left_brace _r*_ {}"; "class _l_ no_left_brace _r*_ {}";
-      "declare () ;"; "echo vvar ext"; "echo _l_ no_semicolon _r*_ ;"; "exit ;";
-      "foreach () {}"; "foreach () :  _l_ no_breach _r*_  endforeach ;";
-      "@ id () ;"; "function id () {}"; "return  function () {} ;";
-      "include_like _l_ stringy _r*_ ;"; "interface _l_ no_left_brace _r*_ {}";
-      "if () beheaded_ivy"; "if () : beheaded_iwy endif ;"; "namespace nmspc ;";
-      "namespace id ;"; "namespace  namespace_name {}";
-      "return _l_ no_semicolon _r*_ ;"; "namespace  {}"; "ext";
-      " id :: id () _l_ -> id () _r+_  ;";
-      "vvar _l_ -> id_or_var optional_pblock  _r+_ ;";
-      "static vvar assign assignable ;"; "id :: id () ;"; "nmspc :: id () ;";
-      "switch () {}"; "trait id {}"; "try {} catch () {}"; "while () {}";
-      "use _l_ no_semicolon _r*_ ;"
-    ];   
+
+  "beheaded_ivy",[
+                   "exit ;";
+                   "{}   _l_ else if () {} _r*_      else {} "
+                 ];
+
+  "beheaded_iwy",[
+                   "_l_ no_ivies _r*_ if () : _l_ no_ivies _r*_ else : _l_ no_ivies _r*_ endif ; _l_ no_ivies _r*_";
+                   "_l_ no_ivies _r*_ if () : _l_ no_ivies _r*_ endif ; _l_ no_ivies _r*_";
+                   "_l_ no_ivies _r*_"
+                 ];
+
+  "statement",[
+                "vvar [ ] = assignable ;";
+                "vvar assign & assignable ;";
+                "vvar -> id_or_var  =  assignable ;";
+                "id :: id_or_var =  assignable ;";
+                "vvar assign  assignable ;";
+                "vvar [  int_or_string_or_var  ] = assignable ;";
+                "vvar [  int_or_string_or_var   ]  =   & assignable  ;";
+                "abstract class _l_ no_left_brace _r*_ {}";
+                "final class _l_ no_left_brace _r*_ {}";
+                "class _l_ no_left_brace _r*_ {}";
+                "declare () ;";
+                "echo vvar ext";
+                "echo _l_ no_semicolon _r*_ ;";
+                "exit ;";
+                "foreach () {}";
+                "foreach () :  _l_ no_breach _r*_  endforeach ;";
+                "@ id () ;";
+                "function id () {}";
+                "return  function () {} ;";
+                "include_like _l_ stringy _r*_ ;";
+                "interface _l_ no_left_brace _r*_ {}";
+                "if () beheaded_ivy";
+                "if () : beheaded_iwy endif ;";
+                "namespace nmspc ;";
+                "namespace id ;";
+                "namespace  namespace_name {}";
+                "return _l_ no_semicolon _r*_ ;";
+                "namespace  {}";
+                "ext";
+                " id :: id () _l_ -> id () _r+_  ;";
+                "vvar _l_ -> id_or_var optional_pblock  _r+_ ;";
+                "static vvar assign assignable ;";
+                "id :: id () ;";
+                "nmspc :: id () ;";
+                "switch () {}";
+                "trait id {}";
+                "try {} catch () {}";
+                "while () {}";
+                "use _l_ no_semicolon _r*_ ;"
+              ]
+
+
 ]);;
+
 
 (* Definition of PHP spider ends here *)
 
@@ -81,13 +109,13 @@ let php ()=unveil(!php_ref);;
 let print_stringlist_naively l=
   let temp1=Image.image (fun s->(Strung.enclose s)) l in
   let temp2=String.concat ";" temp1 in
-  "["^temp2^"]\n";;  
+  "["^temp2^"]";;  
 
 let print_stringlist_with_offset l w=
     let offset=String.make w ' ' in
     let temp1=Image.image (fun s->offset^"  "^(Strung.enclose s)) l in
-    let temp2=("[")::(temp1@[offset^"]\n"]) in
-    String.concat "\n" temp2;;
+    let temp2=String.concat ";\n" temp1 in
+    ("[\n")^temp2^"\n"^offset^"]";;
 
 let print_stringlist w l=
      if List.length(l)=1
@@ -104,11 +132,11 @@ let print_spider_item (s,l)=
 
 let print_spider (Sp l)=
    let temp1=Image.image print_spider_item l in
-   let temp2=String.concat "\n" temp1 in
-   "["^temp2^"]";;
+   let temp2=String.concat ";\n\n" temp1 in
+   temp2;;
 
 let helper_for_rememberance new_spider=
-  "\n\n\n let php_ref=ref(Sp\n"^(print_spider new_spider)^");;\n\n\n";;
+  "\n\n\n let php_ref=ref(Sp[\n"^(print_spider new_spider)^"\n\n\n]);;\n\n\n";;
 
 end;;  
   
