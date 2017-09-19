56b022259c45b2cd38d5897a409a9bf7bce53c24
ewan-delanoy <ewan.delanoy@gmx.fr>
2017-09-19 14:18:48 +0200

    modified Php_named_recognizer.

--- Php_analizer/php_named_recognizer.ml
+++ Php_analizer/php_named_recognizer.ml
@@ -6,12 +6,15 @@ Adds names to PHP recognizers.
 
 *)
 
+
+
 type t=
   {
-    name : string;
+    names : string list;
     definition : string;
+    shortened_definition : string option;
     unnamed_content : Php_constructible_recognizer.t;
-    divided : t list;
+    elements : t list;
     is_a_chain : bool;
     is_a_disjunction : bool;
   };;
@@ -25,17 +28,21 @@ module Private=struct
     let max_name_length=100;;
     let original_data=(Image.image(
       fun (s,sel)->{
-        name =s;
+        names =[s];
         definition=s;
+        shortened_definition=None;
         unnamed_content=Php_constructible_recognizer.leaf(sel);
-        divided=[];
+        elements=[];
         is_a_chain=false;
         is_a_disjunction=false;
       }
     ) Php_short_selector.readables_and_selectors);; 
     let data=ref(original_data);;
-    let encode elt=
-         (elt.name,(elt.definition,elt.unnamed_content,elt.divided));;
+
+    let principal_name nr=List.hd(nr.names);;
+
+    let encode nr=
+         (principal_name nr,nr.unnamed_content);;
     let order =((
       fun elt1 elt2->
         Total_ordering.for_longest_match_pairs (encode elt1) (encode elt2)
@@ -49,61 +56,80 @@ module Private=struct
          let _=(automatic_name_counter:=j) in
          "recognizer_"^(string_of_int(j));;
 
-    let compute_name (opt_name,definition)=
+    let compute_name opt_name=
         match opt_name with
          Some(nahme)->(if String.length(nahme)>max_name_length
                     then  raise(Name_too_long(nahme))
                     else 
-                    if List.exists(fun x->x.name=nahme)(!data)
+                    if List.exists(fun x->List.mem nahme x.names)(!data)
                     then raise(Name_already_in_use(nahme))
                     else nahme)
-        |None->if (String.length(definition)>max_name_length)     
-                  ||
-                  (List.exists(fun x->x.name=definition)(!data))
-               then new_automatic_name ()
-               else definition;;     
+        |None->new_automatic_name ();;     
+      
+      let force_add_name_to_element nahme x =
+        let new_x= 
+        {
+          names =x.names;
+          definition=x.definition;
+          shortened_definition=x.shortened_definition;
+          unnamed_content=x.unnamed_content;
+          elements=x.elements;
+          is_a_chain=x.is_a_chain;
+          is_a_disjunction=x.is_a_disjunction;
+        } in
+        let new_data=Image.image (fun y->if y.names=x.names then new_x else y) (!data) in
+        let _=(data:=new_data) in
+        new_x;;
 
-    let make (opt_name,defn,rcgzr,div)=
+    let make (opt_name,defn,short_defn,rcgzr,elts)=
          match Option.seek(fun nr->nr.unnamed_content=rcgzr) (!data) with
-         Some(nr1)->nr1
+         Some(nr1)->(
+               match opt_name with
+               None->nr1
+              |Some(nahme)->
+                  if List.mem nahme nr1.names 
+                  then nr1
+                  else force_add_name_to_element nahme nr1
+              )
          |None->
          let x={
-          name =compute_name (opt_name,defn);
+          names =[compute_name (opt_name)];
           definition=defn;
+          shortened_definition=short_defn;
           unnamed_content=rcgzr;
-          divided=div;
+          elements=elts;
           is_a_chain=(Php_constructible_recognizer.chain_content(rcgzr)<>None);
           is_a_disjunction=(Php_constructible_recognizer.disjunction_content(rcgzr)<>None);
          }  in
          let _=(data:=Ordered.insert_plaen order x (!data)) in
          x;; 
     
-    let generalized opt_name grlzr nr=
+    let generalized (opt_name,short_defn) grlzr nr=
           let (lpar,rpar)=Generalizer.pair grlzr in
-          let definition=lpar^" "^(nr.name)^" "^rpar in
+          let definition=lpar^" "^(principal_name nr)^" "^rpar in
           let rcgzr=Php_constructible_recognizer.generalized
              grlzr nr.unnamed_content in  
-          make (opt_name,definition,rcgzr,[]);;   
+          make (opt_name,definition,short_defn,rcgzr,[]);;   
              
-    let chain opt_name old_l_nr=
+    let chain (opt_name,short_defn) old_l_nr=
           let temp1=Image.image (
               fun nr->if nr.is_a_chain 
-                      then nr.divided
+                      then nr.elements
                       else [nr]
           ) old_l_nr in
           let l_nr=List.flatten temp1 in
           if List.length(l_nr)=1
           then List.hd l_nr
           else 
-          let definition=String.concat " " (Image.image (fun nr->nr.name) l_nr) in
+          let definition=String.concat " " (Image.image principal_name l_nr) in
           let rcgzr=Php_constructible_recognizer.chain
           (Image.image (fun nr->nr.unnamed_content) l_nr) in  
-           make (opt_name,definition,rcgzr,l_nr);;   
+           make (opt_name,definition,short_defn,rcgzr,l_nr);;   
         
-    let disjunction opt_name old_l_nr=
+    let disjunction (opt_name,short_defn) old_l_nr=
       let temp1=Image.image (
         fun nr->if nr.is_a_disjunction 
-                then nr.divided
+                then nr.elements
                 else [nr]
       ) old_l_nr in
       let l_nr=List.flatten temp1 in     
@@ -111,46 +137,44 @@ module Private=struct
       let definition=
         lpar^
         (String.concat Php_symbols_for_recognizer_description.associator_for_disjunction 
-          (Image.image (fun nr->nr.name) l_nr))
+          (Image.image principal_name l_nr))
         ^rpar
       in
       let rcgzr=Php_constructible_recognizer.disjunction
       (Image.image (fun nr->nr.unnamed_content) l_nr) in  
-       make (opt_name,definition,rcgzr,l_nr);;   
+       make (opt_name,definition,short_defn,rcgzr,l_nr);;   
 
     exception Unknown_name of string;;
 
     let of_name nahme=
-      try Option.find(fun nr->nr.name=nahme)(!data) with
+      try Option.find(fun nr->List.mem nahme nr.names)(!data) with
       Option.Unpackable(_)->raise(Unknown_name(nahme));;
 
-    let of_elementary_definition opt_name defn=
-      let names=Image.image (fun nr->nr.name) (!data) in
+    let of_elementary_definition (opt_name,short_defn) defn=
+      let names=List.flatten(Image.image (fun nr->nr.names) (!data)) in
       let temp1=Strung.longest_match_parsing names defn in
       let temp2=Image.image of_name temp1 in
-      chain opt_name temp2;;
-    
-
+      chain (opt_name,short_defn) temp2;;
+  
     exception Helper_for_definition_reading_exn of ((string*string) option)*string;;
     
-    
-    let helper_for_definition_reading opt_name (opt,t)=
-                let oed=of_elementary_definition in
-                if opt=None then oed opt_name t else
+    let helper_for_definition_reading name_options (opt,t)=
+                let oed=of_elementary_definition  in
+                if opt=None then oed name_options t else
                 let pair=Option.unpack opt in
                 let opt2=Option.seek
                   (fun x->(Generalizer.pair x)=pair)
                   Generalizer.all in
                 if opt2<>None 
-                then generalized opt_name (Option.unpack opt2) (oed None t) 
+                then generalized name_options (Option.unpack opt2) (oed (None,None) t) 
                 else
                 if pair=Php_symbols_for_recognizer_description.pair_for_disjunction
                 then 
                      let temp1=Parenthesed_block.decompose_with_associator
                                 Php_symbols_for_recognizer_description.associator_for_disjunction 
                                 Php_symbols_for_recognizer_description.all_pairs t in
-                     disjunction opt_name (Image.image 
-                          (oed None) temp1)
+                     disjunction name_options (Image.image 
+                          (oed (None,None))  temp1)
                 else
                 raise(Helper_for_definition_reading_exn(opt,t));; 
     
@@ -158,7 +182,7 @@ module Private=struct
     exception Complicated of string*((((string*string) option)*string) list);; 
         
                 
-    let of_definition opt_name rough_s=
+    let of_definition name_options rough_s=
         let s=Cull_string.trim_spaces rough_s in
         if s="" then raise(Empty_output) else
         let temp1=Parenthesed_block.decompose_without_taking_blanks_into_account 
@@ -169,16 +193,10 @@ module Private=struct
         then raise(Empty_output)
         else
         if List.length(temp3)=1
-        then helper_for_definition_reading opt_name (List.hd temp3)
+        then helper_for_definition_reading name_options (List.hd temp3)
         else  
-        let temp4=Image.image (helper_for_definition_reading None) temp3 in
-        chain opt_name temp4;;            
-
-    let official_defs=ref([]:(string*string) list);;
-    
-    let make_official_def x y=
-        (official_defs:=(x,y)::(!official_defs);
-         of_definition (Some x) y);;
+        let temp4=Image.image (helper_for_definition_reading (None,None)) temp3 in
+        chain name_options temp4;;            
 
     let rec iterator_for_apparition_order (graet,names,da_ober)=
         if da_ober=[]
@@ -186,24 +204,26 @@ module Private=struct
         else 
         let tester=(fun nr->
            List.for_all (fun t->
-             Ordered_string.elfenn t.name names
-           ) nr.divided
+             Ordered_string.elfenn (principal_name t) names
+           ) nr.elements
         ) in
         let (dead_ones,still_alive)=List.partition tester da_ober in
-        let dead_names=Image.image (fun nr->nr.name) dead_ones in
+        let dead_names=List.flatten(Image.image (fun nr->nr.names) dead_ones) in
         let updated_names=Ordered_string.teuzin names (Ordered_string.diforchan dead_names) in
         iterator_for_apparition_order (dead_ones::graet,updated_names,still_alive);;
     
     let absorb_spider_item (item_name,l)=
-         if List.length l=1
-         then make_official_def item_name (List.hd l)
+         let n=List.length l in
+         if n=1
+         then of_definition (Some(item_name),None) (List.hd l)
          else
          let temp1=Ennig.index_everything(l) in
          let temp2=Image.image(fun (j,s)->
               let tj=item_name^"_"^(string_of_int j) in
-              make_official_def tj s
+              of_definition (Some(tj),None) s
          ) temp1 in
-         disjunction (Some item_name) temp2;;
+         let shortened_def=item_name^"_i (1<=i<="^(string_of_int n)^")" in
+         disjunction (Some item_name,Some shortened_def) temp2;;
     
 
 
@@ -215,17 +235,6 @@ module Private=struct
 
     let add_dependencies x=(Php_spider.add_dependencies x;reset_with_usual ());;
     let remove_dependencies x=(Php_spider.remove_dependencies x;reset_with_usual ());;
-    
-    let of_official_name old_name=
-       match Option.seek(fun nr->nr.name=old_name)(!data) with
-       Some(nr)->nr
-       |None->
-       let defn=(
-           match Option.seek (fun (x,y)->x=old_name) (!official_defs) with
-           Some(_,y)->y
-           |None->old_name
-       ) in
-       of_definition (Some old_name) defn;;
 
     let analize_item s=
        let temp1=List.assoc s (Php_spider.php()) in
@@ -236,7 +245,7 @@ module Private=struct
        let temp3=Image.image(
            fun (j,t)->
               let sj=s^"_"^(string_of_int j) in
-              let named_rcgzr=of_official_name sj in
+              let named_rcgzr=of_name sj in
               let rcgzr=named_rcgzr.unnamed_content in
               (t,fst(Php_constructible_recognizer.big_head_tail_decomposition rcgzr))
        ) temp2 in
@@ -251,18 +260,19 @@ module Private=struct
 end;;
 
 
-let generalized=Private.generalized;;
-let chain=Private.chain;;
-let disjunction=Private.disjunction;;
 
-let of_name=Private.of_official_name;;
-let of_definition=Private.of_definition;;
+let name=Private.principal_name;;
+let of_name=Private.of_name;;
+let definition x= match x.shortened_definition with
+                  Some(sdef)->sdef
+                  |None->x.definition;;
+let of_definition=Private.of_definition (None,None);;
 
 
 let chain_content nr=
    if Php_constructible_recognizer.chain_content(nr.unnamed_content)=None
    then None  
-   else Some(nr.divided);;
+   else Some(nr.elements);;
 
 let is_constant nr=Php_constructible_recognizer.is_constant 
                     nr.unnamed_content;;
@@ -279,7 +289,7 @@ let basic_parser nr=((function l->
 let star_parser nr=Php_parser_homomorphism.star (basic_parser nr);;
 
 let eat_prechewed x l= 
-  let temp1=of_definition None x in
+  let temp1=of_definition x in
   let temp2=temp1.unnamed_content in
   Php_constructible_recognizer.recognize temp2 l;;
 
@@ -298,7 +308,7 @@ let remove_dependencies=Private.remove_dependencies;;
 let analize_item=Private.analize_item;;
 
 let print (x:t)=
-    "\xc9\xbe  "^(x.name)^"  \xc9\xbf";;
+    "\xc9\xbe  "^(name x)^" : "^(definition x)^"  \xc9\xbf";;
   
 let print_out (fmt:Format.formatter) (x:t)=
      Format.fprintf fmt "@[%s@]" (print x);;
