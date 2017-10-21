835375a841b78a66ca1ec2d61f54bd5ae2aad7c6
ewan-delanoy <ewan.delanoy@gmx.fr>
2017-10-21 16:26:03 +0200

    modified Namespacize.

--- Php_analizer/Great_Replacement/namespacize.ml
+++ Php_analizer/Great_Replacement/namespacize.ml
@@ -4,26 +4,6 @@
 
 *)
 
-let after_whites_and_comments s=
-  let n=String.length s in
-  let rec tempf=(
-    fun j->
-      if j>n then None else
-      if List.mem (String.get s (j-1)) list_of_whites
-      then tempf(j+1)
-      else 
-      if Substring.is_a_substring_located_at "/*" s j
-      then let k=Substring.leftmost_index_of_in_from "*/" s (j+2) in
-           if k<0
-           then None
-           else tempf(k+2)
-      else Some(j)
-  ) in
-  tempf;;
-
-(*    
-after_whites_and_comments "\n/* 567 */\t\r\n\n/* 89 ** // 78*/123";;    
-*)
 
 
 module Private=struct
@@ -40,6 +20,28 @@ let after_whites s =
       else Some(j)
   ) in
   tempf;;
+
+  let after_whites_and_comments s=
+    let n=String.length s in
+    let rec tempf=(
+      fun j->
+        if j>n then None else
+        if List.mem (String.get s (j-1)) list_of_whites
+        then tempf(j+1)
+        else 
+        if Substring.is_a_substring_located_at "/*" s j
+        then let k=Substring.leftmost_index_of_in_from "*/" s (j+2) in
+             if k<0
+             then None
+             else tempf(k+2)
+        else Some(j)
+    ) in
+    tempf;;
+  
+  (*    
+  after_whites_and_comments "\n/* 567 */\t\r\n\n/* 89 ** // 78*/123";;    
+  *)
+  
 
 exception Unbalanced_expression of char*char;;
 
@@ -300,6 +302,9 @@ let standardize ap=
 Io.erase_file_and_fill_it_with_string 
 ap new_content;;
 
+let after_whites_and_comments=Private.after_whites_and_comments;;
+let after_closing_character=Private.after_closing_character;;
+let decompose_from=Private.decompose_from;;
 
 let expand_inclusion
       inserted_file
