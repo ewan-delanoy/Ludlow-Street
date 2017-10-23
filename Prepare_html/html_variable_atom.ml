28c191e0783322f3c45c70f912000f3f1fb1f58f
ewan-delanoy <ewan.delanoy@gmx.fr>
2017-10-23 09:28:26 +0200

    modified Html_variable_atom.

--- Prepare_html/html_variable_atom.ml
+++ Prepare_html/html_variable_atom.ml
@@ -8,3 +8,15 @@ type t=
    Simple of Html_parameter_type.t*((string*string) list) 
    |Unregistered of ((Html_parameter_value.t list) -> string);;
 
+exception Missing_parameter of Html_parameter_type.t * (Html_parameter_value.t list);;  
+
+let apply x l=
+  match x with
+   Simple(typ,l_assoc)->
+         (
+           match Option.seek (fun vaal->Html_parameter_value.get_type vaal=typ) l with
+           None->raise(Missing_parameter(typ,l))
+           |Some(vaal)->List.assoc (Html_parameter_value.get_content vaal) l_assoc
+         )
+  |Unregistered(f)->f l;;
+
