13ce8a700064c3ed92137cb9b83132e1f627444c
ewan-delanoy <ewan.delanoy@gmx.fr>
2017-10-23 09:13:12 +0200

    modified Html_variable_atom.

--- Prepare_html/html_variable_atom.ml
+++ Prepare_html/html_variable_atom.ml
@@ -4,8 +4,7 @@
 
 *)
 
-(*
 type t=
    Simple of Html_parameter_type.t*((string*string) list) 
-   |Unregistered of (Html_parameter_value.t list) -> string;;
-*)
+   |Unregistered of ((Html_parameter_value.t list) -> string);;
+
