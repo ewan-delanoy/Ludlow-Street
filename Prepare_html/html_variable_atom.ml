70ff8bb84028a9fac8db27698e8fe1fbab9762db
ewan-delanoy <ewan.delanoy@gmx.fr>
2017-10-27 09:42:27 +0200

    deleted Html_parameter_value. Created Html_parameter. Modified Html_atom,Html_variable_atom.

--- Prepare_html/html_variable_atom.ml
+++ Prepare_html/html_variable_atom.ml
@@ -6,17 +6,17 @@
 
 type t=
    Simple of Html_parameter_type.t*((string*string) list) 
-   |Unregistered of ((Html_parameter_value.t list) -> string);;
+   |Unregistered of ((Html_parameter.t list) -> string);;
 
-exception Missing_parameter of Html_parameter_type.t * (Html_parameter_value.t list);;  
+exception Missing_parameter of Html_parameter_type.t * (Html_parameter.t list);;  
 
 let apply x l=
   match x with
    Simple(typ,l_assoc)->
          (
-           match Option.seek (fun vaal->Html_parameter_value.get_type vaal=typ) l with
+           match Option.seek (fun vaal->Html_parameter.get_type vaal=typ) l with
            None->raise(Missing_parameter(typ,l))
-           |Some(vaal)->List.assoc (Html_parameter_value.get_content vaal) l_assoc
+           |Some(vaal)->List.assoc (Html_parameter.get_content vaal) l_assoc
          )
   |Unregistered(f)->f l;;
 
