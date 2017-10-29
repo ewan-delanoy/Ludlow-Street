6bdf8ccc2020ab7240d492b1c4365b9c283f7004
ewan-delanoy <ewan.delanoy@gmx.fr>
2017-10-29 09:32:45 +0100

    modified After.

--- after.ml
+++ after.ml
@@ -38,6 +38,49 @@ let after_whites s =
   after_whites_and_comments "\n/* 567 */\t\r\n\n/* 89 ** // 78*/123";;    
   *)
   
+exception Unfinished_simple_quoted_string of int;;  
+
+let after_simple_quoted_string s k0=
+    let n=String.length s in
+    if (Strung.get s k0)<>'\''
+    then k0
+    else 
+    let rec tempf=(fun k->
+       if k>n
+       then raise(Unfinished_simple_quoted_string(k0))
+       else 
+       let c=String.get s (k-1) in
+       if c='\\'
+       then tempf(k+2)
+       else 
+       if c='\''
+       then k+1
+       else tempf(k+1)
+    ) in
+    tempf (k0+1);;
+
+exception Unfinished_double_quoted_string of int;;  
+    
+let after_double_quoted_string s k0=
+        let n=String.length s in
+        if (Strung.get s k0)<>'"'
+        then k0
+        else 
+        let rec tempf=(fun k->
+           if k>n
+           then raise(Unfinished_double_quoted_string(k0))
+           else 
+           let c=String.get s (k-1) in
+           if c='\\'
+           then tempf(k+2)
+           else 
+           if c='"'
+           then k+1
+           else tempf(k+1)
+        ) in
+        tempf (k0+1);;     
+
+
 
 exception Unbalanced_expression of char*char;;
 
@@ -48,10 +91,28 @@ let after_closing_character (lchar,rchar) s=
       if k>n
       then raise(Unbalanced_expression(lchar,rchar))
       else 
+      if Substring.is_a_substring_located_at "/*" s k
+      then let j=Substring.leftmost_index_of_in_from "*/" s (k+2) in
+           tempf(j+2,count)
+      else 
+      if (Substring.is_a_substring_located_at "<<<EOF\n" s k)
+         ||
+         (Substring.is_a_substring_located_at "<<<'EOF'\n" s k) 
+      then let j=Substring.leftmost_index_of_in_from "\nEOF;\n" s (k+7) in
+           tempf(j+6,count)
+      else 
       let c=String.get s (k-1) in
       if c=lchar
       then tempf(k+1,count+1)
       else 
+      if c='\''
+      then let j=after_simple_quoted_string s k in
+           tempf(j,count)
+      else
+      if c='"'
+      then let j=after_double_quoted_string s k in
+           tempf(j,count)
+      else     
       if c<>rchar
       then tempf(k+1,count)
       else 
@@ -65,6 +126,11 @@ let after_closing_character (lchar,rchar) s=
 
 after_closing_character ('{','}') "{ 345 }89" (1,0);;
 after_closing_character ('{','}') "{2{4}6{8{0}2}4}67" (1,0);;
+after_closing_character ('{','}') "{\"3}5\"}89" (1,0);;
+after_closing_character ('{','}') "{'3}5'}89" (1,0);;
+after_closing_character ('{','}') "{/*4}6*/}01" (1,0);;
+after_closing_character ('{','}') "{<<<EOF\n}\nEOF;\n}78" (1,0);;
+after_closing_character ('{','}') "{<<<'EOF'\n}\nEOF;\n}90" (1,0);;
 
 *)
 
