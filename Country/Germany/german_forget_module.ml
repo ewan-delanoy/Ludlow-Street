6a482c7becf2b44c747e00c344558e9440ce4377
ewan-delanoy <ewan.delanoy@gmx.fr>
2017-10-31 13:18:26 +0100

    modified German_forget_module.

--- Country/Germany/german_forget_module.ml
+++ Country/Germany/german_forget_module.ml
@@ -9,6 +9,8 @@ exception ModuleWithDependencies of
 	Half_dressed_module.t*(Half_dressed_module.t list);;
 exception Non_registered_module of Half_dressed_module.t;;
 
+let ralph=ref([]);;
+
 let on_targets (mdata,dirs,tgts) hm=
   match Alaskan_data.find_module_registration mdata hm with
    None->raise(Non_registered_module(hm))
@@ -19,7 +21,10 @@ let on_targets (mdata,dirs,tgts) hm=
          let fn=(Directory_name.connectable_to_subpath(German_constant.root))^s_hm in
          let (answer,short_paths)=German_unregister_module.on_targets (mdata,tgts) hm in
          let _=Image.image
-         (fun edg->Unix_command.uc("rm -f _build/"^fn^edg))
+         (fun edg->
+          let cmd="rm -f _build/"^fn^edg in
+          let _=(ralph:=(!ralph)@[cmd]) in 
+          Unix_command.uc(cmd))
          [".cm*";".d.cm*";".caml_debuggable"] in
          let temp1=Image.image (fun t->
             Absolute_path.of_string(Directory_name.join (German_constant.root) t)
