c07f92134fab5b26e947f82c3701df5350560fcd
ewan-delanoy <ewan.delanoy@gmx.fr>
2017-11-08 07:38:17 +0100

    created Html_bracable.

---
+++ Prepare_html/html_bracable.ml
@@ -0,0 +1,20 @@
+(*
+
+#use"Prepare_html/html_bracable.ml";;
+
+*)
+
+
+type t=
+   Simple of (Html_parameter.t*string) list
+  |Uncategorized of ((Html_parameter.t list)->string);;
+
+exception Missing_parameter;;
+
+let eval l_param =function
+  Simple(l_assoc)->
+       ( match Option.seek(fun (key,vval)->List.mem key l_param) l_assoc  with
+         None->raise(Missing_parameter)
+         |Some(_,vval)->vval
+       )
+  |Uncategorized (f)->f l_param;;    
