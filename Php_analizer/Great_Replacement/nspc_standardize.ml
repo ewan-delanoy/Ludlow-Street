a4c2999748affb65ad1b854bc5827290730ec36a
ewan-delanoy <ewan.delanoy@gmx.fr>
2017-11-09 14:34:30 +0100

    modified Nspc_standardize.

--- Php_analizer/Great_Replacement/nspc_standardize.ml
+++ Php_analizer/Great_Replacement/nspc_standardize.ml
@@ -4,19 +4,27 @@
 
 *)
 
+let insert_end_padding text=
+    if Substring.ends_with text "\n\n"
+    then text
+    else text^"\n\n";;
+
 let namespaced_item item=
     let (nspc_line,nspc_content,offset,after_nspc)=item in
      if offset=""
      then let new_nspc_line=Replace_inside.replace_inside_string
                (";","{") nspc_line in
-          (new_nspc_line,nspc_content,"}",after_nspc) 
+          (new_nspc_line,insert_end_padding nspc_content,"}",after_nspc) 
      else item;;    
 
 let decomposed_form dec_form=
     let before_namespaces=Nspc_decomposed_form.before_namespaces dec_form in
     match  Nspc_decomposed_form.namespacable dec_form with
-    Some(text)->Nspc_decomposed_form.make before_namespaces
-                   None ["namespace {",text,"}",""]
+    Some(text)->
+                 Nspc_decomposed_form.make before_namespaces
+                   None ["namespace {",
+                         insert_end_padding text,
+                         "}",""]
     |None->
     let items=Nspc_decomposed_form.namespaced_parts dec_form in
     let new_items=Image.image namespaced_item items in
