6d1b7624d98986f6e7487c2fc4e293e77cb4aeed
ewan-delanoy <ewan.delanoy@gmx.fr>
2017-11-15 16:41:36 +0100

    modified Merge_paragraphs.

--- Text_editing/merge_paragraphs.ml
+++ Text_editing/merge_paragraphs.ml
@@ -6,6 +6,11 @@
 
 let has_suspicious_beginning paragraph=
       let temp=Cull_string.trim_spaces_on_the_left paragraph in
+      if List.exists(
+        fun beg->Substring.begins_with temp beg
+      ) ["\195\168";"\195\169"]
+      then true
+      else 
       let i=int_of_char(String.get temp 0) in
       (97<=i)&&(i<=122);;
 
