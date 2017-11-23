d9d3b6f1b55cffe1ee7501c959d22173db1bdb11
ewan-delanoy <ewan.delanoy@gmx.fr>
2017-11-23 15:22:21 +0100

    modified Charset.

--- charset.ml
+++ charset.ml
@@ -4,15 +4,18 @@
 
 *)
 
-
+let lowercase_letter=    
+  ['a';'b';'c';'d';'e';'f';'g';'h';'i';'j';
+   'k';'l';'m';'n';'o';'p';'q';'r';'s';'t';
+   'u';'v';'w';'x';'y';'z'];;
 
     
- let uppercase_letters= 
+let uppercase_letters= 
    ['A';'B';'C';'D';'E';'F';'G';'H';'I';'J';
     'K';'L';'M';'N';'O';'P';'Q';'R';'S';'T';
     'U';'V';'W';'X';'Y';'Z'];;
     
- let lowercase_identifier_elements=    
+let lowercase_identifier_elements=    
     ['a';'b';'c';'d';'e';'f';'g';'h';'i';'j';
      'k';'l';'m';'n';'o';'p';'q';'r';'s';'t';
      'u';'v';'w';'x';'y';'z';'_';'+';'-';'*';
