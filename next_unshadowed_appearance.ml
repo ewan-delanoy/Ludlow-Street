8bf913cdfcb2d9e2ffcb25c3e90f74718bd39204
ewan-delanoy <ewan.delanoy@gmx.fr>
2017-11-27 15:10:38 +0100

    created Next_unshadowed_appearance. Modified After.

---
+++ next_unshadowed_appearance.ml
@@ -0,0 +1,53 @@
+(*
+
+#use"next_unshadowed_appearance.ml";;
+
+*)
+
+let test_for_perfect_match s k kwd=
+   if not(Substring.is_a_substring_located_at kwd s k)
+   then false
+   else 
+   let constraint1=(
+       if k<2 then true else
+       not(List.mem (Strung.get s (k-1)) 
+       Charset.strictly_alphanumeric_characters )
+   )   in
+   if not(constraint1)
+   then false
+   else
+   let n=String.length(s) and m=k+(String.length kwd) in
+   if m>n
+   then true
+   else not(List.mem (Strung.get s m) 
+        Charset.strictly_alphanumeric_characters );;
+
+let nua s l_kwds=
+  let n=String.length s in
+  let rec tempf=(fun k->
+   if k>n then None else
+   if List.exists (test_for_perfect_match s k) l_kwds
+   then Some(k)         
+   else 
+   if Substring.is_a_substring_located_at "/*" s k
+   then let m=Substring.leftmost_index_of_in_from "*/" s (k+2) in
+        tempf m
+   else
+   let c=Strung.get s k in
+   if c='\''
+   then let m=After.after_simple_quoted_string s k in tempf m
+   else
+   if c='"'
+   then let m=After.after_double_quoted_string s k in tempf m
+   else tempf (k+1)
+  ) in
+  tempf;;
+
+(*
+
+nua "/* include */ \"require_once\" 'include_once' require 3" 
+["include_once";"require_once";"include";"require"]
+1;;
+
+*)
+
