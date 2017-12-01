0d07cb66f1a37a9223e4e1a2acd260e5cf33fead
ewan-delanoy <ewan.delanoy@gmx.fr>
2017-12-01 16:13:40 +0100

    modified Classlike_kind.

---
+++ Php_analizer/Great_Replacement/classlike_kind.ml
@@ -0,0 +1,20 @@
+(*
+
+#use"Php_analizer/Great_Replacement/classlike_kind.ml";;
+
+
+*)
+
+type t=
+      Abstract_class
+     |Final_class
+     |Usual_class 
+     |Interface
+     |Plain_text;;
+
+let abstract_class=Abstract_class;;
+let final_class=Final_class;;
+let usual_class=Usual_class;;
+let itface=Interface;;
+let plain_text=Plain_text;;
+
