531e99ace53d62c862599b8741c0657e5cac6423
ewan-delanoy <ewan.delanoy@gmx.fr>
2017-12-07 14:28:06 +0100

    created Functionlike_kind.

---
+++ Php_analizer/Great_Replacement/functionlike_kind.ml
@@ -0,0 +1,23 @@
+(*
+
+#use"Php_analizer/Great_Replacement/functionlike_kind.ml";;
+
+
+*)
+
+type t=
+  Non_function
+ |Usual_function 
+ |Private_method
+ |Protected_method
+ |Public_method 
+ |Namespace_line
+ |After_namespace_comments;;
+
+let non_function = Non_function;;
+let usual_function = Usual_function;;
+let private_method = Private_method;;
+let protected_method = Protected_method;;
+let public_method = Public_method;;
+let namespace_line =  Namespace_line;;
+let after_namespace_comments = After_namespace_comments;;
