0c2f52b656e48bdc64d1a6c78f2dd16d21a56846
ewan-delanoy <ewan.delanoy@gmx.fr>
2018-01-20 15:33:20 +0100

    created Atomic_hrecognizer.

---
+++ Php_analizer/HRecognizer/atomic_hrecognizer.ml
@@ -0,0 +1,24 @@
+(*
+
+#use"Php_analizer/HRecognizer/atomic_hrecognizer.ml";;
+
+*)
+
+type t=
+   Constant of string
+  |Later_constant of string
+  |Constant_list of string list
+  |Later_constant_list of string list
+  |Exactly_one of char list
+  |Star of char list
+  |Enclosed of char*char;;
+
+let constant s=Constant(s);;
+let later_constant s=Later_constant(s);;
+let constant_list l= Constant_list(l);;
+let later_constant_list l=Later_constant_list(l);;
+let exactly_one l=Exactly_one(l);;
+let star l=Star(l);;
+let enclosed (opener,closer)=Enclosed (opener,closer);;
+
+
