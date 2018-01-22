8e34b3a90e2af0ec68a1bed6c9a3ee8f26aa171f
ewan-delanoy <ewan.delanoy@gmx.fr>
2018-01-22 16:33:47 +0100

    modified Atomic_hrecognize.

--- Php_analizer/HRecognizer/atomic_hrecognize.ml
+++ Php_analizer/HRecognizer/atomic_hrecognize.ml
@@ -4,6 +4,8 @@
 
 *)
 
+module Private=struct
+
 let recgnz_constant text s i=
   if not(Substring.is_a_substring_located_at text s i)
   then None
@@ -63,11 +65,13 @@ let recgnz x=
   |Atomic_hrecognizer.Star(l_chr)->recgnz_star l_chr
   |Atomic_hrecognizer.Enclosed(opener,closer)->recgnz_enclosed (opener,closer);;
 
+end;;
+
 let rec chain_in_detail (s,i_start,current_i,graet,da_ober)=
   match da_ober with
   []->(Some(current_i),None)
   |atom::peurrest->
-    match recgnz atom s current_i with
+    match Private.recgnz atom s current_i with
     None->(None,Some(List.rev graet,atom,peurrest)) 
     |Some(j)->chain_in_detail (s,i_start,j,current_i::graet,peurrest);;
 
