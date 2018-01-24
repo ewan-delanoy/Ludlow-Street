5910125deecf3e6c6a7f4e676d323a385dd883dd
ewan-delanoy <ewan.delanoy@zoho.com>
2018-01-24 07:31:36 +0100

    modified Html_hedgehog_pack.

--- Prepare_html/Tag_related/html_hedgehog_pack.ml
+++ Prepare_html/Tag_related/html_hedgehog_pack.ml
@@ -62,11 +62,22 @@ let add_closing_tag ((i:int),j,tag_name) (P l_hedgehog)=
         else P(new_hedgehog1::peurrest)
     );;
 
+let is_not_a_closable_tag t=
+  (Substring.begins_with t "!DOCTYPE")
+  ||
+  (List.mem t ["br"]);;
+
+
 let add_tag (i,j,tag_descr) hpack=
    if Substring.is_a_substring_located_at "/" tag_descr 2
    then add_opening_tag 
         (i,j,Cull_string.interval tag_descr 3 (j-i)) hpack
-   else add_opening_tag 
+   else let t=Cull_string.interval tag_descr 2 (j-i) in
+        if is_not_a_closable_tag t
+        then add_string_constant 
+            (i,j,tag_descr) hpack
+        else 
+        add_opening_tag 
         (i,j,Cull_string.interval tag_descr 2 (j-i)) hpack ;;
 
 exception Cannot_simplify_multipack of Html_hedgehog.t list;;
