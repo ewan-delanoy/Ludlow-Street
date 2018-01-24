9f4c4f731993bc2a84d53cd4b09f4885058f6eae
ewan-delanoy <ewan.delanoy@zoho.com>
2018-01-24 14:38:32 +0100

    created Parametric_hrecognize.

---
+++ Php_analizer/HRecognizer/parametric_hrecognize.ml
@@ -0,0 +1,23 @@
+(*
+
+#use"Php_analizer/HRecognizer/parametric_hrecognize.ml";;
+
+*)
+
+
+
+let rec chain_in_detail (s,i_start,current_i,graet,da_ober)=
+  match da_ober with
+  []->(Some(current_i,graet),None)
+  |atom::peurrest->
+    match Atomic_hrecognize.recgnz atom s current_i with
+    None->(None,Some(List.rev graet,atom,peurrest)) 
+    |Some(j)->chain_in_detail (s,i_start,j,current_i::graet,peurrest);;
+
+let chain label l_atoms s i=
+  let (opt1,opt2)=chain_in_detail (s,i,i,[],l_atoms) in
+  match opt1 with
+  Some(last_i,indexes)->Some(label,indexes,last_i)
+  |None->None;;    
+
+  
