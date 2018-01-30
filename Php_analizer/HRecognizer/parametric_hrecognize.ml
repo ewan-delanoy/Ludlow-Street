329617592937b751feb2145389ebf08f87672f60
ewan-delanoy <ewan.delanoy@zoho.com>
2018-01-30 14:44:37 +0100

    modified Parametric_hrecognize.

--- Php_analizer/HRecognizer/parametric_hrecognize.ml
+++ Php_analizer/HRecognizer/parametric_hrecognize.ml
@@ -8,14 +8,14 @@
 
 let rec chain_in_detail (s,i_start,current_i,graet,da_ober)=
   match da_ober with
-  []->(Some(current_i,graet),None)
+  []->(Some(current_i,List.rev graet),None)
   |atom::peurrest->
     match Atomic_hrecognize.recgnz atom s current_i with
     None->(None,Some(List.rev graet,atom,peurrest)) 
     |Some(j)->chain_in_detail (s,i_start,j,current_i::graet,peurrest);;
 
 let chain label l_atoms s i=
-  let (opt1,opt2)=chain_in_detail (s,i,i,[],l_atoms) in
+  let (opt1,_)=chain_in_detail (s,i,i,[],l_atoms) in
   match opt1 with
   Some(last_i,indexes)->Some(label,indexes,last_i)
   |None->None;;    
