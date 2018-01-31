4bb4af5bae0fdae75892f32371a1fdf3c42df524
ewan-delanoy <ewan.delanoy@zoho.com>
2018-01-31 14:16:05 +0100

    modified Atomic_hrecognizer.

--- Php_analizer/HRecognizer/atomic_hrecognizer.ml
+++ Php_analizer/HRecognizer/atomic_hrecognizer.ml
@@ -15,6 +15,7 @@ type t=
  |Constant_list of string list
  |Later_constant_list of string list
  |Exactly_one of char list
+ |Star of char list
  |Nonempty_Star of char list
  |Nonempty_Star_outside of char list
  |Enclosed of char*char;;
@@ -24,6 +25,7 @@ let later_constant s=Later_constant(s);;
 let constant_list l= Constant_list(l);;
 let later_constant_list l=Later_constant_list(l);;
 let exactly_one l=Exactly_one(l);;
+let star l=Star(l);;
 let nonempty_star l=Nonempty_Star(l);;
 let nonempty_star_outside l=Nonempty_Star_outside(l);;
 let enclosed (opener,closer)=Enclosed (opener,closer);;
