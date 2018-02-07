e52250886c1f4ac600d614e794d8753020427ea2
ewan-delanoy <ewan.delanoy@zoho.com>
2018-02-07 09:35:24 +0100

    modified Nonatomic_hrecognizer.

--- Php_analizer/HRecognizer/nonatomic_hrecognizer.ml
+++ Php_analizer/HRecognizer/nonatomic_hrecognizer.ml
@@ -1,17 +1,14 @@
 (*
-
 #use"Php_analizer/HRecognizer/nonatomic_hrecognizer.ml";;
-
 *)
 
 type t=
-   Leaf of string*Atomic_hrecognizer.t
-  |Chain of string*(t list)
-  |Ordered_disjunction of string*(t list)
-  |Star of string*t;;
+  Leaf of string*Atomic_hrecognizer.t
+ |Chain of string*(t list)
+ |Ordered_disjunction of string*(t list)
+ |Star of string*t;;
 
 let leaf s x=Leaf(s,x);;
 let chain s l=Chain(s,l);;
 let ordered_disjunction s l= Ordered_disjunction(s,l);;
 let star s l=Star(s,l);; 
-
