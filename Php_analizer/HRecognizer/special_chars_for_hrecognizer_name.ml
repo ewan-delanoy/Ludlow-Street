2ef92953aed632b94f7f7aa5806ce56c28d4de23
ewan-delanoy <ewan.delanoy@zoho.com>
2018-02-08 14:54:18 +0100

    created Special_chars_for_hrecognizer_name.

---
+++ Php_analizer/HRecognizer/special_chars_for_hrecognizer_name.ml
@@ -0,0 +1,13 @@
+(*
+
+#use"Php_analizer/HRecognizer/special_chars_for_hrecognizer_name.ml";;
+
+*)
+
+let chain_announcer="2";;
+let disjunction_announcer="8";;
+let star_announcer="5";;
+
+let chain_separator="0";;
+let disjunction_separator="1";;
+
