319c105ca79990eabbae9badb4afdfa3be36fb79
ewan-delanoy <ewan.delanoy@zoho.com>
2018-02-20 12:09:58 +0100

    modified Atomic_hrecognize.

--- Php_analizer/HRecognizer/atomic_hrecognize.ml
+++ Php_analizer/HRecognizer/atomic_hrecognize.ml
@@ -75,6 +75,14 @@ let recgnz_enclosed (opener,closer) s i=
   else 
   Some(After.after_closing_character (opener,closer) s (i+1,1));;
  
+let recgnz_simple_quoted s i=
+  try (Some(After.after_simple_quoted_string s i)) with
+  _->None;;
+
+let recgnz_double_quoted s i=
+    try (Some(After.after_simple_quoted_string s i)) with
+    _->None;;
+
 let recgnz x=
   match x with
    Atomic_hrecognizer.Constant(s)->recgnz_constant s
@@ -84,7 +92,13 @@ let recgnz x=
   |Atomic_hrecognizer.Star(l_chr)->recgnz_star l_chr
   |Atomic_hrecognizer.Nonempty_Star(l_chr)->recgnz_nonempty_star l_chr
   |Atomic_hrecognizer.Nonempty_Star_outside(l_chr)->recgnz_nonempty_star_outside l_chr
-  |Atomic_hrecognizer.Enclosed(opener,closer)->recgnz_enclosed (opener,closer);;
+  |Atomic_hrecognizer.Enclosed(opener,closer)->recgnz_enclosed (opener,closer)
+  |Atomic_hrecognizer.Simple_quoted->recgnz_simple_quoted
+  |Atomic_hrecognizer.Double_quoted->recgnz_double_quoted
+  ;;
 
 
+
+
+  
   
