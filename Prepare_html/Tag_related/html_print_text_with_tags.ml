b7c39c512af990dcf33b3f513d4c15801aed4d1d
ewan-delanoy <ewan.delanoy@zoho.com>
2018-02-22 09:24:53 +0100

    modified More_coherent_pdf,Html_print_text_with_tags.

--- Prepare_html/Tag_related/html_print_text_with_tags.ml
+++ Prepare_html/Tag_related/html_print_text_with_tags.ml
@@ -11,8 +11,12 @@ let rec compute_lines j text=match text with
     let temp1=Str.split (Str.regexp_string "\n") s in
     let indent=String.make j ' ' in
     Image.image (fun t->indent^t) temp1 
-|Html_text_with_tags.Tagged(_,_,x)->
-    compute_lines(j+tab_width) x
+|Html_text_with_tags.Tagged(tag_name,_,x)->
+    let indent=String.make j ' ' in
+    let first_line=indent^"<"^full_tag^">"
+    and last_line= indent^"</"^tag_name^">" in
+    let temp1=compute_lines(j+tab_width) x in
+    first_line::(temp1@[last_line])
 |Html_text_with_tags.Concat(l)->
   let temp2=Image.image (compute_lines j) l in
   List.flatten temp2;;
--- Text_editing/more_coherent_pdf.ml
+++ Text_editing/more_coherent_pdf.ml
@@ -88,7 +88,7 @@ let example=chunk
 let example=More_coherent_pdf.chunk
  ~rootdir:"/Users/ewandelanoy/Documents/html_files/Printable"
  ~pdfname:"agreda"
- ~interval:(141,160)
+ ~interval:(161,180)
  ~chunksize:None;;
 
 let res1=chunk example;; 
