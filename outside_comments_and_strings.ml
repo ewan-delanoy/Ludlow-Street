8a229a49adeb27a26487f249051be7c8b1cff5e0
ewan-delanoy <ewan.delanoy@zoho.com>
2018-04-28 10:36:12 +0200

    created Krecognize. Modified Outside_comments_and_strings,Check_hrecognizers_disjointness.

--- outside_comments_and_strings.ml
+++ outside_comments_and_strings.ml
@@ -10,9 +10,7 @@ Comments are a little more complicated than strings because they
 can be nested. Also, note that we can have strings inside comments :
 for example (* a " ( * " b *) is a valid OCaml code snippet.
 
-*)
 
-(*
 To keep the automaton simple, changes are notified as soon as
 possible. Thus, the automaton toggles string_mode or changes
 nesting comment depth as soon as the terminating character is
@@ -26,6 +24,7 @@ type state={
     lastchar_is_a_left_paren          : bool;
     lastchar_is_a_star                : bool;
     lastchar_is_a_backslash           : bool;
+    lastchar_is_a_tick           : bool;
     rightmost_backslash_count_is_even : bool;
     penultchar_is_a_left_paren        : bool;
     interval_start : int;
@@ -54,7 +53,8 @@ let one_more_step s n j c x=
    
    ) in
    let string_opened_now=(c='"')&&(not(x.string_mode))&&
-       (not(x.lastchar_is_a_backslash))
+       (not(x.lastchar_is_a_backslash))&&
+       (not(x.lastchar_is_a_tick))
    and string_closed_now=(c='"')&&(x.string_mode)&&(
       if x.lastchar_is_a_backslash
       then x.rightmost_backslash_count_is_even
@@ -100,6 +100,7 @@ let one_more_step s n j c x=
     lastchar_is_a_left_paren   =(c='(');
     lastchar_is_a_star         =(c='*');
     lastchar_is_a_backslash    =(c='\\');
+    lastchar_is_a_tick    =(c='\'');
     rightmost_backslash_count_is_even=(if c<>'\\' 
                                        then true 
                                        else not(x.rightmost_backslash_count_is_even) );
@@ -115,6 +116,7 @@ let initial_state=
     lastchar_is_a_left_paren   =false;
     lastchar_is_a_star         =false;
     lastchar_is_a_backslash    =false;
+    lastchar_is_a_tick    =false;
     rightmost_backslash_count_is_even=true;
     penultchar_is_a_left_paren =false;
     interval_start=1;
@@ -147,7 +149,7 @@ good_substrings "ghi(**a(*b*)c**)def";;
 good_substrings "ghi(**a\"b\"c**)def";;
 good_substrings "123\"(*\"890\"*)\"567";;
 good_substrings "123(*67\"90\"23*)67";;
-
+good_substrings "let a=7;; let b='\"';; let c=8;;";;
 
 let nachste (s,n,j,st)=(s,n,j+1,one_more_step s n j (String.get s (j-1)) st);;
 let s0="123456\"\\\\\"123456789";;
