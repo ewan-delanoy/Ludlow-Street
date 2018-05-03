56e428cb8c3efe2423f8d482e81bfc13370c4ce7
ewan-delanoy <ewan.delanoy@zoho.com>
2018-05-03 13:35:57 +0200

    modified Abstractified_nonatomic_hrecognizer.

--- Php_analizer/HRecognizer/abstractified_nonatomic_hrecognizer.ml
+++ Php_analizer/HRecognizer/abstractified_nonatomic_hrecognizer.ml
@@ -8,9 +8,7 @@ but I couldn't find anything better so far.
 *)
 
 type t=
-  Leaf of string*Atomic_hrecognizer.t
- |Chain of string*(string list)
- |Ordered_disjunction of string*(string list)
+  Chain of string*(string list)
  |Star of string*string
  |Maybe of string*string
  |Keyword_avoider of string*string*Avoider_label.t
@@ -18,9 +16,7 @@ type t=
  |Disjunction_of_chains of string*(string list list);;
 
  let name=function
-  Leaf(s,_)->s
- |Chain(s,_)->s
- |Ordered_disjunction(s,_)->s
+  Chain(s,_)->s
  |Star(s,_)->s
  |Maybe(s,_)->s
  |Keyword_avoider(s,_,_)->s
@@ -28,9 +24,7 @@ type t=
  |Disjunction_of_chains(s,_)->s;;
 
  let support=function
-  Leaf(_,_)->[]
- |Chain(_,l)->l
- |Ordered_disjunction(_,l)->l
+  Chain(_,l)->l
  |Star(_,s1)->[s1]
  |Maybe(_,s1)->[s1]
  |Keyword_avoider(_,s1,_)->[s1]
