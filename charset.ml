4e76b698e6a06a31fd7cb4883b48f3e7cbd9662d
ewan-delanoy <ewan.delanoy@zoho.com>
2018-05-04 12:41:23 +0200

    modified Charset.

--- charset.ml
+++ charset.ml
@@ -39,6 +39,13 @@ let php_label_first_letters =
    '0';'1';'2';'3';'4';'5';'6';'7';'8';'9'
   ];;   
 
+let ocaml_modulename_nonfirst_letters=
+  ['a'; 'b'; 'c'; 'd'; 'e'; 'f'; 'g'; 'h'; 'i'; 'j'; 'k'; 'l'; 'm'; 'n'; 'o';
+ 'p'; 'q'; 'r'; 's'; 't'; 'u'; 'v'; 'w'; 'x'; 'y'; 'z'; 'A'; 'B'; 'C'; 'D';
+ 'E'; 'F'; 'G'; 'H'; 'I'; 'J'; 'K'; 'L'; 'M'; 'N'; 'O'; 'P'; 'Q'; 'R'; 'S';
+ 'T'; 'U'; 'V'; 'W'; 'X'; 'Y'; 'Z'; '_'; '0'; '1'; '2'; '3'; '4'; '5'; '6';
+ '7'; '8'; '9'];;
+
 let alphanumeric_characters =
   php_label_nonfirst_letters @
   [
