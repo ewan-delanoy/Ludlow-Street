d238c3dac035a513626dc0894f74472cae02f29d
ewan-delanoy <ewan.delanoy@zoho.com>
2018-06-01 07:55:45 +0200

    created Follow_ocaml_values.

--- Ocaml_analysis/follow_ocaml_values.ml
+++ Ocaml_analysis/follow_ocaml_values.ml
@@ -12,30 +12,39 @@ let order_for_string_pairs =
   (string*string) Total_ordering.t
   );;
 
+let followed_values_from_items l=
+    let temp1=Option.filter_and_unpack (
+       fun x->
+         match x.Ocaml_gsyntax_item.category with
+         Ocaml_gsyntax_category.Value                                                                          
+      | Ocaml_gsyntax_category.Type
+      | Ocaml_gsyntax_category.Exception->
+              Some(x.Ocaml_gsyntax_item.name,x.Ocaml_gsyntax_item.content)
+      | _->None
+  ) l in
+  let temp2=Ordered.diforchan_plaen order_for_string_pairs temp1 in
+  Followed_ocaml_values.F(temp2);;
+
 end;;
 
-let follow_values l=
-  let temp1=Option.filter_and_unpack (
-     fun x->
-       match x.Ocaml_gsyntax_item.category with
-       Ocaml_gsyntax_category.Value                                                                          
-    | Ocaml_gsyntax_category.Type
-    | Ocaml_gsyntax_category.Exception->
-            Some(x.Ocaml_gsyntax_item.name,x.Ocaml_gsyntax_item.content)
-    | _->None
-) l in
-let temp2=Ordered.diforchan_plaen Private.order_for_string_pairs temp1 in
-Followed_ocaml_values.F(temp2);;
 
-(*
+
+
 let local_delchacre (Followed_ocaml_values.F l1) (Followed_ocaml_values.F l2)=
-   let names_from_l1=Image.image fst l1
-   and names_from_l2=Image.image fst l2 in
-   let list_of_all_names=Ordered.teuzin_plaen  
-   Total_ordering.lex_for_strings names_from_l1 names_from_l2 in
-   let m=List.length list_of_all_names in
-   let array_of_all_names=Array.of_list list_of_all_names in
-*)
+  let (common,in_l1_only,in_l2_only)=Ordered.diff_plaen  Total_ordering.lex_for_strings 
+    ([],[],[],l1,l2) in
+  (
+    Image.image fst in_l1_only, 
+   Option.filter_and_unpack (fun
+    (name,content1,content2)->
+     if content1=content2 then None else Some(name)
+    ) common,
+    Image.image fst in_l2_only
+  );;
+  
+let follow_values hm=Private.followed_values_from_items(
+  Read_ocaml_file_without_expanding_inclusions.rofwei hm
+);;
 
 
 
