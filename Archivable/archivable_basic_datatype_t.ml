02987f679b3eac111c266ef58bc775f6517e20b5
ewan-delanoy <ewan.delanoy@zoho.com>
2018-06-08 15:21:45 +0200

    created Archivable_basic_datatype_t.

---
+++ Archivable/archivable_basic_datatype_t.ml
@@ -0,0 +1,13 @@
+(*
+
+#use"Archivable/archivable_basic_datatype_t.ml";;
+
+Archivable means that there is a uniform to_string function.
+
+*)
+
+type t=
+     Integer
+    |Boolean
+    |Float
+    |String;;
