5f05283e9160d636309c583334950cd87a500082
ewan-delanoy <ewan.delanoy@zoho.com>
2018-09-10 13:16:05 +0200

    modified Coma_state.

--- Makefile_makers/coma_state.ml
+++ Makefile_makers/coma_state.ml
@@ -2309,17 +2309,19 @@ let rename_module x old_name new_name=
           set_targets x new_tgts;
          );;    
 
-let pre_start_debugging root_dir (mdata,tgts)=
-    let _=Alaskan_remove_debuggables.rd root_dir mdata in
-    let dbg=Coma_constant.name_for_debugged_module in
+let pre_start_debugging mdata=
+  let root_dir=mdata.Coma_state_t.root in
+  let _=Alaskan_remove_debuggables.rd root_dir mdata in
+  let dbg=Coma_constant.name_for_debugged_module in
 	let rdir=compute_subdirectories_list mdata in
 	let ap=Find_suitable_ending.find_file_location root_dir rdir 
 	     (dbg^".ml") in
 	let hm=Half_dressed_module.of_path_and_root ap root_dir in
-	let mdata2=recompute_module_info mdata hm in
+  let mdata2=recompute_module_info mdata hm in
+  let old_tgts=mdata.Coma_state_t.targets in
 	let tgt=Ocaml_target.debuggable hm in
 	let answer=Ocaml_target_making.make root_dir
-	(mdata2,tgts,[]) tgt in
+	(mdata2,old_tgts,[]) tgt in
 	let msg=(
 	  if (fst answer)
 	  then "\n\n Now, start \n\nocamldebug _build/"^dbg^".ocaml_debuggable\n\nin another terminal\n\n"
@@ -2334,8 +2336,7 @@ let pre_start_debugging root_dir (mdata,tgts)=
 
 let start_debugging x=
           let (bowl,(new_mdata,new_tgts,_))=
-            pre_start_debugging (root x)
-            (x,targets x)  in
+            pre_start_debugging x  in
           if bowl
           then (
             set_targets x new_tgts 
